34ea2a7a7046709f6d3018a56463116c9337c37e
Se0 <54990890+onetuks@users.noreply.github.com>
2024-09-28 01:00:58 +0900

    Title: 100을 넘기는 순간, Time: 113ms, Memory: 24MB, Status: Passed - CodeTree

--- 240928/100을 넘기는 순간/as-soon-as-hit-100.py
+++ 240928/100을 넘기는 순간/as-soon-as-hit-100.py
@@ -3,11 +3,13 @@ n = int(input())
 nums = list(map(int, input().split()))
 
 acc_sum = 0
+cnt = 0
 
 for num in nums:
     acc_sum += num 
+    cnt += 1
     if num >= 100:
         break
 
 print(acc_sum)
-print(round(acc_sum / n, 1))
+print(round(acc_sum / cnt, 1))
