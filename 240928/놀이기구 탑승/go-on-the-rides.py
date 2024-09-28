f1c78d163d0ec9d43f0eec4d5a365aaad8aee277
Se0 <54990890+onetuks@users.noreply.github.com>
2024-09-28 11:03:37 +0900

    Title: 놀이기구 탑승, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 240928/놀이기구 탑승/go-on-the-rides.py
+++ 240928/놀이기구 탑승/go-on-the-rides.py
@@ -1,60 +1,75 @@
-dlist = [(0, 1), (0, -1), (1, 0), (-1, 0)]
+dlist = [(-1, 0), (1, 0), (0, -1), (0, 1)]
 
-def seat(num):
+n = 0
+students = dict()
+matrix = []
+
+def seat(seq):
     loc = [-1, -1]
     max_like_cnt = 0
     max_void_cnt = 0
     for i in range(n):
         for j in range(n):
-            if matrix[i][j] > 0:
+            if matrix[i][j] != 0:
                 continue
             like_cnt = 0
             void_cnt = 0
             for dx, dy in dlist:
                 di, dj = i + dx, j + dy
-                if 0 <= di < n and 0 <= dj < n:
-                    if matrix[di][dj] == -1:
-                        void_cnt += 1
-                    elif matrix[di][dj] in students[num]:
-                        like_cnt += 1
+                if di < 0 or di >= n or dj < 0 or dj >= n:
+                    continue
+                if matrix[di][dj] == 0:
+                    void_cnt += 1
+                elif matrix[di][dj] in students[seq]:
+                    like_cnt += 1
             if like_cnt > max_like_cnt:
                 max_like_cnt = like_cnt
+                max_void_cnt = void_cnt
                 loc = [i, j]
-            if like_cnt == max_like_cnt and void_cnt > max_void_cnt:
+            elif like_cnt == max_like_cnt and void_cnt > max_void_cnt:
                 max_void_cnt = void_cnt
                 loc = [i, j]
     return loc
 
 
-def get_like_cnt(i, j):
+
+def get_cnt(i, j):
+    num = matrix[i][j]
     cnt = 0
     for dx, dy in dlist:
         di, dj = i + dx, j + dy
-        if 0 <= di < n and 0 <= dj < n and matrix[di][dj] in students[matrix[i][j]]:
+        if 0 <= di < n and 0 <= dj < n and matrix[di][dj] in students[num]:
             cnt += 1
     return cnt
 
 
-n = int(input())
+def main():
+    global n, students, matrix
 
-students = dict()
-sequence = list()
+    n = int(input())
 
-for _ in range(n * n):
-    line = list(map(int, input().split()))
-    students[line[0]] = line[1:]
-    sequence.append(line[0])
+    sequence = list()
+    matrix = [[0 for _ in range(n)] for _ in range(n)]
+    
+    for _ in range(n * n):
+        line = list(map(int, input().split()))
+        students[line[0]] = line[1:]
+        sequence.append(line[0])
 
-matrix = [[-1 for _ in range(n)] for _ in range(n)]
+    for seq in sequence:
+        x, y = seat(seq)
+        matrix[x][y] = seq
 
-for st_num in sequence:
-    loc = seat(st_num)
-    matrix[loc[0]][loc[1]] = st_num
+        # for mat in matrix:
+        #     print(mat)
+        # print()
 
-total_score = 0
-for i in range(n):
-    for j in range(n):
-        like_cnt = get_like_cnt(i, j)
-        total_score += int(pow(10, like_cnt - 1))
+    answer = 0
+    for i in range(n):
+        for j in range(n):
+            cnt = get_cnt(i, j)
+            answer += int(pow(10, cnt - 1))
 
-print(total_score)
+    print(answer)
+
+main()
