64353882f29f88b61ade11c4b2ce14a72cbb1662
Se0 <54990890+onetuks@users.noreply.github.com>
2024-09-30 16:24:36 +0900

    Title: 함께가는 열차, Time: 275ms, Memory: 29MB, Status: Passed - CodeTree

--- 240930/함께가는 열차/a-train-that-goes-together.py
+++ 240930/함께가는 열차/a-train-that-goes-together.py
@@ -1,20 +1,13 @@
-'''
-Greedy
-
-1. 순회하면서 속도가 점점 높은 곳을 찾기
-'''
-
 n = int(input())
 trains = [list(map(int, input().split())) for _ in range(n)]
-
 trains.sort()
 
-cnt = 1
-min_vel = trains[-1][1]
+cnt = 0
+min_vel = trains[-1][-1]
 
-for i in range(n - 2, -1, -1):
-    if trains[i][1] <= min_vel:
-        min_vel = trains[i][1]
+for i in reversed(range(n)):
+    if trains[i][-1] <= min_vel:
         cnt += 1
+        min_vel = trains[i][-1]
 
 print(cnt)
