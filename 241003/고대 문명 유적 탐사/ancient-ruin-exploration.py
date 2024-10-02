2ea77ee9959cded5d0d54b9883bc34985751eb6c
Se0 <54990890+onetuks@users.noreply.github.com>
2024-10-03 00:47:17 +0900

    Title: 고대 문명 유적 탐사, Time: 408ms, Memory: 28MB, Status: Passed - CodeTree

--- 241003/고대 문명 유적 탐사/ancient-ruin-exploration.py
+++ 241003/고대 문명 유적 탐사/ancient-ruin-exploration.py
@@ -1,80 +1,81 @@
 from collections import deque
 from copy import deepcopy
 
-dlist = [(0, 1), (0, -1), (1, 0), (-1, 0)]
-
-def rotate(i, j, r, matrix):
-    result = deepcopy(matrix)
-    sub = [[result[i + x][j + y] for y in range(3)] for x in range(3)]
-    for _ in range(r):
-        sub = rotate_sub(sub)
-    for x in range(3):
-        for y in range(3):
-            result[i + x][j + y] = sub[x][y]
+def rotate(sy, sx, cnt):
+    result = deepcopy(board)
+    for _ in range(cnt):
+        tmp = result[sy + 0][sx + 2]
+        result[sy + 0][sx + 2] = result[sy + 0][sx + 0]
+        result[sy + 0][sx + 0] = result[sy + 2][sx + 0]
+        result[sy + 2][sx + 0] = result[sy + 2][sx + 2]
+        result[sy + 2][sx + 2] = tmp
+        tmp = result[sy + 1][sx + 2]
+        result[sy + 1][sx + 2] = result[sy + 0][sx + 1]
+        result[sy + 0][sx + 1] = result[sy + 1][sx + 0]
+        result[sy + 1][sx + 0] = result[sy + 2][sx + 1]
+        result[sy + 2][sx + 1] = tmp
     return result
 
-
-def rotate_sub(sub):
-    return list(zip(*sub[::-1]))
-
-
-def cal_cnt(board):
-    result = 0
-    visited = [[False] * 5 for _ in range(5)]
-
+def cal_score(board): 
+    score = 0
+    # 방문여부 체크
+    visit = [[False] * 5 for _ in range(5)]
+    # BFS 방향
+    dy, dx = [0, 1, 0, -1], [1, 0, -1, 0]
+    
     for i in range(5):
         for j in range(5):
-            if not visited[i][j]:
-                visited[i][j] = True
-                dq = deque([(i, j)])
-                trace = deque([(i, j)])
+            if not visit[i][j]:
+                dq, trace = deque([(i, j)]), deque([(i, j)])
+                visit[i][j] = True
                 while dq:
-                    x, y = dq.popleft()
-                    for dx, dy in dlist:
-                        di, dj = x + dx, y + dy
-                        if 0 <= di < 5 and 0 <= dj < 5 and not visited[di][dj] and board[di][dj] == board[x][y]:
-                            visited[di][dj] = True
-                            dq.append((di, dj))
-                            trace.append((di, dj))
+                    cur = dq.popleft()
+                    for k in range(4):
+                        ny, nx = cur[0]+dy[k], cur[1]+dx[k]
+                        if 0<=ny<5 and 0<=nx<5 and visit[ny][nx] == False and board[ny][nx] == board[cur[0]][cur[1]]:
+                            dq.append((ny, nx))
+                            trace.append([ny, nx])
+                            visit[ny][nx] = True
                 if len(trace) >= 3:
-                    result += len(trace)
+                    score += len(trace)
                     while trace:
-                        x, y = trace.popleft()
-                        board[x][y] = 0
-    return result
+                        t = trace.popleft()
+                        board[t[0]][t[1]] = 0
+    return score
 
-
-def fill(board):
+def fill(b, qq):
     for j in range(5):
-        for i in reversed(range(5)):
-            if board[i][j] == 0:
-                board[i][j] = mlist.popleft()
-    return board
+        for i in range(4, -1, -1):
+            if b[i][j] == 0:
+                b[i][j] = qq.popleft()
+    return b
 
+K, M = map(int, input().split())
+board = []
+for i in range(5):
+    board.append(list(map(int, input().split())))
+M_list = list(map(int, input().split()))
+M_list = deque(M_list)
 
-k, m = map(int, input().split())
-matrix = [list(map(int, input().split())) for _ in range(5)]
-mlist = deque(map(int, input().split()))
-
-for _ in range(k):
-    max_cnt = 0
-    max_matrix = list()
-    for r in range(1, 4):
-        for i in range(3):
-            for j in range(3):
-                rotated = rotate(i, j, r, matrix)
-                cnt = cal_cnt(rotated)
-                if cnt > max_cnt:
-                    max_cnt = cnt
-                    max_matrix = rotated
-    if max_cnt == 0:
+for _ in range(K):
+    maxScore = 0
+    maxScoreBoard = None
+    for cnt in range(1, 4):
+        for j in range(3):
+            for i in range(3):
+                rotated = rotate(i, j, cnt)
+                score = cal_score(rotated)
+                if maxScore < score:
+                    maxScore = score
+                    maxScoreBoard = rotated
+    if maxScoreBoard is None:
         break
-    matrix = max_matrix
+    board = maxScoreBoard
     while True:
-        matrix = fill(matrix)
-        cnt = cal_cnt(matrix)
-        if cnt == 0:
+        board = fill(board, M_list)
+        newScore = cal_score(board)
+        if newScore == 0:
             break
-        max_cnt += cnt
+        maxScore += newScore
 
-    print(max_cnt, end=" ")
+    print(maxScore, end=" ")
