f9e5eae2e31eb2a084e62f98d41cdeed049a48ed
Se0 <54990890+onetuks@users.noreply.github.com>
2024-10-03 15:49:48 +0900

    Title: 고대 문명 유적 탐사, Time: 176ms, Memory: 25MB, Status: Wrong Answer - CodeTree

--- 241003/고대 문명 유적 탐사/ancient-ruin-exploration.py
+++ 241003/고대 문명 유적 탐사/ancient-ruin-exploration.py
@@ -1,81 +1,86 @@
 from collections import deque
-from copy import deepcopy
 
-def rotate(sy, sx, cnt):
-    result = deepcopy(board)
-    for _ in range(cnt):
-        tmp = result[sy + 0][sx + 2]
-        result[sy + 0][sx + 2] = result[sy + 0][sx + 0]
-        result[sy + 0][sx + 0] = result[sy + 2][sx + 0]
-        result[sy + 2][sx + 0] = result[sy + 2][sx + 2]
-        result[sy + 2][sx + 2] = tmp
-        tmp = result[sy + 1][sx + 2]
-        result[sy + 1][sx + 2] = result[sy + 0][sx + 1]
-        result[sy + 0][sx + 1] = result[sy + 1][sx + 0]
-        result[sy + 1][sx + 0] = result[sy + 2][sx + 1]
-        result[sy + 2][sx + 1] = tmp
-    return result
+nb, ns = 5, 3
 
-def cal_score(board): 
-    score = 0
-    # 방문여부 체크
-    visit = [[False] * 5 for _ in range(5)]
-    # BFS 방향
-    dy, dx = [0, 1, 0, -1], [1, 0, -1, 0]
-    
-    for i in range(5):
-        for j in range(5):
-            if not visit[i][j]:
-                dq, trace = deque([(i, j)]), deque([(i, j)])
-                visit[i][j] = True
-                while dq:
-                    cur = dq.popleft()
-                    for k in range(4):
-                        ny, nx = cur[0]+dy[k], cur[1]+dx[k]
-                        if 0<=ny<5 and 0<=nx<5 and visit[ny][nx] == False and board[ny][nx] == board[cur[0]][cur[1]]:
-                            dq.append((ny, nx))
-                            trace.append([ny, nx])
-                            visit[ny][nx] = True
-                if len(trace) >= 3:
-                    score += len(trace)
-                    while trace:
-                        t = trace.popleft()
-                        board[t[0]][t[1]] = 0
-    return score
+class Board:
+    def __init__(self):
+        self.a = [[0 for _ in range(nb)] for _ in range(nb)]
 
-def fill(b, qq):
-    for j in range(5):
-        for i in range(4, -1, -1):
-            if b[i][j] == 0:
-                b[i][j] = qq.popleft()
-    return b
+    def rotate(self, si, sj, cnt):
+        result = Board()
+        result.a = [row[:] for row in self.a]
+        for _ in range(cnt):
+            temp = result.a[si + 0][sj + 2]
+            result.a[si + 0][sj + 2] = result.a[si + 0][sj + 0]
+            result.a[si + 0][sj + 0] = result.a[si + 2][sj + 0]
+            result.a[si + 2][sj + 0] = result.a[si + 2][sj + 2]
+            result.a[si + 2][sj + 2] = temp
+            temp = result.a[si + 1][sj + 2]
+            result.a[si + 1][sj + 2] = result.a[si + 0][sj + 1]
+            result.a[si + 0][sj + 1] = result.a[si + 1][sj + 0]
+            result.a[si + 1][sj + 0] = result.a[si + 2][sj + 1]
+            result.a[si + 2][sj + 1] = temp
+        return result
 
-K, M = map(int, input().split())
-board = []
-for i in range(5):
-    board.append(list(map(int, input().split())))
-M_list = list(map(int, input().split()))
-M_list = deque(M_list)
+    def cal_score(self):
+        score = 0
+        visited = [[False for _ in range(nb)] for _ in range(nb)]
+        dlist = [(0, 1), (0, -1), (1, 0), (-1, 0)]
+        for i in range(nb):
+            for j in range(nb):
+                if not visited[i][j]:
+                    visited[i][j] = True
+                    q, trace = deque([(i, j)]), deque([(i, j)])
+                    while q:
+                        x, y = q.popleft()
+                        for dx, dy in dlist:
+                            di, dj = x + dx, y + dy
+                            if 0 <= di < nb and 0 <= dj < nb and self.a[di][dj] == self.a[x][y] and not visited[di][dj]:
+                                visited[di][dj] = True
+                                q.append((di, dj))
+                                trace.append((di, dj))
+                    if len(trace) >= 3:
+                        score += len(trace)
+                        while trace:
+                            x, y = trace.popleft()
+                            self.a[x][y] = 0
+        return score
 
-for _ in range(K):
-    maxScore = 0
-    maxScoreBoard = None
-    for cnt in range(1, 4):
-        for j in range(3):
+    def fill(self, que):
+        for j in range(nb):
+            for i in reversed(range(nb)):
+                if self.a[i][j] == 0:
+                    self.a[i][j] = que.popleft()
+
+def main():
+    k, m = map(int, input().split())
+    board = Board()
+    for i in range(nb):
+        board.a[i] = list(map(int, input().split()))
+    mlist = deque()
+    for t in list(map(int, input().split())):
+        mlist.append(t)
+
+    for _ in range(k):
+        max_score = 0
+        max_score_board = None
+        for cnt in range(1, 4):
             for i in range(3):
-                rotated = rotate(i, j, cnt)
-                score = cal_score(rotated)
-                if maxScore < score:
-                    maxScore = score
-                    maxScoreBoard = rotated
-    if maxScoreBoard is None:
-        break
-    board = maxScoreBoard
-    while True:
-        board = fill(board, M_list)
-        newScore = cal_score(board)
-        if newScore == 0:
+                for j in range(3):
+                    rotated = board.rotate(i, j, cnt)
+                    score = rotated.cal_score()
+                    if score > max_score:
+                        max_score = score
+                        max_score_board = rotated
+        if max_score_board is None:
             break
-        maxScore += newScore
+        board = max_score_board
+        while True:
+            board.fill(mlist)
+            new_score = board.cal_score()
+            if new_score == 0:
+                break
+            max_score += new_score
+        print(max_score, end=" ")
 
-    print(maxScore, end=" ")
+main()
