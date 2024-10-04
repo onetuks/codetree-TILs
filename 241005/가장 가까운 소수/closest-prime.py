f2cd436d0272f429d58561e03350acb68a88af08
Se0 <54990890+onetuks@users.noreply.github.com>
2024-10-05 00:36:50 +0900

    Title: 가장 가까운 소수, Time: 113ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 241005/가장 가까운 소수/closest-prime.py
+++ 241005/가장 가까운 소수/closest-prime.py
@@ -4,6 +4,8 @@ import math
 input = sys.stdin.readline
 
 def is_prime(num):
+    if num <= 1:
+        return False
     for i in range(2, int(math.sqrt(num)) + 1):
         if num % i == 0:
             return False
@@ -12,11 +14,15 @@ def is_prime(num):
 n = int(input())
 
 for i in range(1, int(1e6) + 1):
-    if n - i < 1 and n + i > int(1e6):
-        break
-    if n - i >= 1 and is_prime(n - i):
-        print(n - i)
-        break
-    elif n + i <= int(1e6) and is_prime(n + i):
-        print(n + i)
+    a, b = n - i, n + i
+    
+    a_prime = is_prime(a)
+    b_prime = is_prime(b)
+
+    if a_prime:
+        print(a, end=" ")
+    if b_prime:
+        print(b)
+    
+    if a_prime or b_prime:
         break
