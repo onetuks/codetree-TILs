7fe0d088e55580060d1b786ef3c63fcb71e9a672
Se0 <54990890+onetuks@users.noreply.github.com>
2024-10-05 14:31:08 +0900

    Title: 세 수의 합, Time: 140ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 241005/세 수의 합/sum-of-three-num.py
@@ -0,0 +1,22 @@
+from collections import defaultdict
+
+n, k = map(int, input().split())
+alist = list(map(int, input().split()))
+dic = defaultdict(int)
+
+for a in alist:
+    dic[a] += 1
+
+answer = 0
+for i in range(n):
+    for j in range(i + 1, n):
+        a = alist[i]
+        b = alist[j]
+        dic[a] -= 1
+        dic[b] -= 1
+        answer += dic[k - a - b]
+        print(answer, a, b, k - a - b, dic[k - a - b])
+        dic[a] += 1
+        dic[b] += 1
+
+print(answer // 3)
