91066b9b2e297a9fa79923849c71002a9d37b81a
Se0 <54990890+onetuks@users.noreply.github.com>
2024-10-05 23:46:16 +0900

    Title: 비율 구하기, Time: 362ms, Memory: 29MB, Status: Passed - CodeTree

---
+++ 241005/비율 구하기/find-proportions.py
@@ -0,0 +1,18 @@
+from sortedcontainers import SortedDict
+
+n = int(input())
+
+words = SortedDict()
+
+for _ in range(n):
+    word = input()
+
+    if word not in words:
+        words[word] = 1
+    else:
+        words[word] += 1
+
+total_cnt = sum(words.values())
+
+for k, v in words.items():
+    print("%s %.4f" %(k, round((v / total_cnt) * 100, 4)))
