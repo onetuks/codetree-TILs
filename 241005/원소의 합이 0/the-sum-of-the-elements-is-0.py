2505a70b8dc6b795760325de6b6f51a8cad9a5c3
Se0 <54990890+onetuks@users.noreply.github.com>
2024-10-05 17:55:17 +0900

    Title: 원소의 합이 0, Time: 375ms, Memory: 53MB, Status: Passed - CodeTree

--- 241005/원소의 합이 0/the-sum-of-the-elements-is-0.py
+++ 241005/원소의 합이 0/the-sum-of-the-elements-is-0.py
@@ -16,15 +16,10 @@ for kc in c.keys():
     for kd in d.keys():
         cd[kc + kd] = c[kc] * d[kd]
 
-ab_keys, cd_keys = sorted(ab.keys()), sorted(cd.keys(), reverse=True)
-
 answer = 0
 
-for ab_key in ab_keys:
-    for cd_key in cd_keys:
-        if ab_key + cd_key == 0:
-            answer += ab[ab_key] * cd[cd_key]
-        elif ab_key + cd_key < 0:
-            break
+for k in ab.keys():
+    if -k in cd:
+        answer += ab[k] * cd[-k]
 
 print(answer)
