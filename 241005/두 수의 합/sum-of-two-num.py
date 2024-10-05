e4d9569229a309b8b333797e634f069f3f80a9d0
Se0 <54990890+onetuks@users.noreply.github.com>
2024-10-05 13:47:59 +0900

    Title: 두 수의 합, Time: 5038ms, Memory: 38MB, Status: Time Limit Exceed - CodeTree

--- 241005/두 수의 합/sum-of-two-num.py
+++ 241005/두 수의 합/sum-of-two-num.py
@@ -4,12 +4,14 @@ n, k = map(int, input().split())
 alist = list(map(int, input().split()))
 
 counter = Counter(alist)
-keys = list(counter.keys())
+keys = sorted(list(counter.keys()))
 
 answer = 0
 
 for i in range(len(keys) - 1):
     for j in range(i + 1, len(keys)):
+        if keys[i] + keys[j] > k:
+            break
         if keys[i] + keys[j] == k:
             answer += counter[keys[i]] * counter[keys[j]]
 
