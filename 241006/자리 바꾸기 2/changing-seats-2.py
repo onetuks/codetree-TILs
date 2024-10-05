ca2070b8706885d76d55775d718549fc8b5a6352
Se0 <54990890+onetuks@users.noreply.github.com>
2024-10-06 00:12:43 +0900

    Title: 자리 바꾸기 2, Time: 108ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 241006/자리 바꾸기 2/changing-seats-2.py
@@ -0,0 +1,14 @@
+n, k = map(int, input().split())
+commands = [tuple(map(int, input().split())) for _ in range(k)]
+
+seats = [i for i in range(n + 1)]
+logs = {i:set([i]) for i in range(1, n + 1)}
+
+for cid in range(3 * k):
+    i, j = commands[cid % k]
+    seats[i], seats[j] = seats[j], seats[i]
+    logs[seats[i]].add(j)
+    logs[seats[j]].add(i)
+
+for i in range(1, n + 1):
+    print(len(logs[i]))
