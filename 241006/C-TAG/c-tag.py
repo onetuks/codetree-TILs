840f4ea4cba659410d904e851e3ecaf010111313
Se0 <54990890+onetuks@users.noreply.github.com>
2024-10-06 22:38:42 +0900

    Title: C-TAG, Time: 967ms, Memory: 25MB, Status: Passed - CodeTree

--- 241006/C-TAG/c-tag.py
+++ 241006/C-TAG/c-tag.py
@@ -1,10 +1,17 @@
 from itertools import combinations
 
-def recognizable(aset, bset):
-    for b in bset:
-        if b in aset:
+def recognizable(pos):
+    aset = set()
+
+    for i in range(n):
+        aset.add(agroup[i][pos[0]] + agroup[i][pos[1]] + agroup[i][pos[2]])
+        
+    for i in range(n):
+        if (bgroup[i][pos[0]] + bgroup[i][pos[1]] + bgroup[i][pos[2]]) in aset:
             return False
+
     return True
+    
 
 n, m = map(int, input().split())
 
@@ -14,12 +21,7 @@ bgroup = [input() for _ in range(n)]
 ans = 0
 
 for perm in combinations(range(m), 3):
-    aset, bset = set(), set()
-    for i in range(n):
-        aset.add(''.join([agroup[i][p] for p in perm]))
-        bset.add(''.join([bgroup[i][p] for p in perm]))
-
-    if recognizable(aset, bset):
+    if recognizable(perm):
         ans += 1
 
 print(ans)
