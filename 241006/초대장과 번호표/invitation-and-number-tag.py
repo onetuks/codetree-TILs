c10f42ba0f050c10c6e0d0be1ed4d9a2e38cd4ff
Se0 <54990890+onetuks@users.noreply.github.com>
2024-10-06 23:52:59 +0900

    Title: 초대장과 번호표, Time: 354ms, Memory: 40MB, Status: Passed - CodeTree

--- 241006/초대장과 번호표/invitation-and-number-tag.py
+++ 241006/초대장과 번호표/invitation-and-number-tag.py
@@ -1,34 +1,29 @@
 from collections import deque, defaultdict
 
 n, g = map(int, input().split())
-member_groups = defaultdict(list)
-groups = dict()
+uninvited = defaultdict(set)
+mem_gids = [list() for _ in range(n + 1)]
 
 for i in range(g):
-    line = list(map(int, input().split()))
+    nums = list(map(int, input().split()))[1:]
 
-    for member in line[1:]:
-        member_groups[member].append(i)
+    for num in nums:
+        uninvited[i].add(num)
+        mem_gids[num].append(i)
 
-    groups[i] = line[1:]
-
-invited = set([1])
 q = deque([1])
+invited = set([1])
 
 while q:
-    member = q.popleft()
+    mem = q.popleft()
 
-    gids = member_groups[member]
-
-    for gid in gids:
-        group = groups[gid]
-        diff = len(group) - len(invited)
-        if diff > 1 or diff < 0:
-            continue
-        rest = set(group) - invited
-        if len(rest) == 1:
-            rest_member = rest.pop()
-            invited.add(rest_member)
-            q.append(rest_member)
+    for gid in mem_gids[mem]:
+        if mem in uninvited[gid]:
+            uninvited[gid].remove(mem)
+        if len(uninvited[gid]) == 1:
+            rest = uninvited[gid].pop()
+            if rest not in invited:
+                invited.add(rest)
+                q.append(rest)
 
 print(len(invited))
