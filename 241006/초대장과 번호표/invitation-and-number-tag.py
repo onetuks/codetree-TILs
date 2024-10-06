f1f32a81a15bab334f62dcf94ba04dc8f3cf866a
Se0 <54990890+onetuks@users.noreply.github.com>
2024-10-06 22:59:52 +0900

    Title: 초대장과 번호표, Time: 813ms, Memory: 31MB, Status: Wrong Answer - CodeTree

--- 241006/초대장과 번호표/invitation-and-number-tag.py
+++ 241006/초대장과 번호표/invitation-and-number-tag.py
@@ -1,16 +1,22 @@
-n, g = map(int, input().split())
-groups = [list(map(int, input().split())) for _ in range(g)]
+from collections import deque
 
-groups.sort(key=lambda x: x)
+n, g = map(int, input().split())
+groups = deque([0] + list(map(int, input().split())) for _ in range(g))
 
 invited = set([1])
 
-for _ in range(2):
-    for group in groups:
-        members = set(group[1:])
-        rest = members - invited
+while groups:
+    group = groups.popleft()
+    members = set(group[2:])
+    rest = members - invited
 
-        if len(rest) <= 1:
-            invited |= members
+    if len(rest) <= 1:
+        invited |= members
+        continue
+    if group[0] > 25:
+        continue
+    group = [group[0] + 1] + group[1:]
+    groups.append(group)
+
 
 print(len(invited))
