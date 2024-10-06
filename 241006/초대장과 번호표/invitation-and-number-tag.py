e3b806f321739b3b2ab45133461e4bf0e09961de
Se0 <54990890+onetuks@users.noreply.github.com>
2024-10-06 23:55:48 +0900

    Title: 초대장과 번호표, Time: 351ms, Memory: 40MB, Status: Passed - CodeTree

--- 241006/초대장과 번호표/invitation-and-number-tag.py
+++ 241006/초대장과 번호표/invitation-and-number-tag.py
@@ -1,29 +1,41 @@
-from collections import deque, defaultdict
+from collections import deque
 
-n, g = map(int, input().split())
-uninvited = defaultdict(set)
-mem_gids = [list() for _ in range(n + 1)]
+# 변수 선언 및 입력:
+n, g = tuple(map(int, input().split()))
+
+invited = [False] * n
+# 각 그룹마다 초대장을 받지 못한 사람들을 관리해줍니다.
+groups = [set() for _ in range(g)]
+# 각 사람이 어떤 그룹에 속하는지를 관리해줍니다.
+people_groups = [[] for _ in range(n)]
+
+q = deque()
+ans = 0
 
 for i in range(g):
     nums = list(map(int, input().split()))[1:]
-
     for num in nums:
-        uninvited[i].add(num)
-        mem_gids[num].append(i)
+        num -= 1
+        groups[i].add(num)
+        people_groups[num].append(i)
 
-q = deque([1])
-invited = set([1])
-
+q.append(0)
+invited[0] = True
 while q:
-    mem = q.popleft()
+    x = q.popleft()
+    ans += 1
 
-    for gid in mem_gids[mem]:
-        if mem in uninvited[gid]:
-            uninvited[gid].remove(mem)
-        if len(uninvited[gid]) == 1:
-            rest = uninvited[gid].pop()
-            if rest not in invited:
-                invited.add(rest)
-                q.append(rest)
+    # x가 들어있는 그룹에서 x를 지웁니다.
+    # hashset에는 그룹에서 초대받지 않은 인원만을 남깁니다.
+    for g_num in people_groups[x]:
+        # 해당 그룹에서 x를 지웁니다.
+        groups[g_num].remove(x)
+        # 초대받지 않은 인원이 한명밖에 없다면 초대합니다.
+        if len(groups[g_num]) == 1:
+            p_num = list(groups[g_num])[0]
+            if not invited[p_num]:
+                invited[p_num] = True
+                q.append(p_num)
 
-print(len(invited))
+# 초대장을 받는 인원을 출력합니다.
+print(ans)
