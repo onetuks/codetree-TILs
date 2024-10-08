cc7da4a5991411a4b128d226be422340a18140ec
Se0 <54990890+onetuks@users.noreply.github.com>
2024-10-08 18:31:12 +0900

    Title: 최대로 연속한 숫자, Time: 3095ms, Memory: 59MB, Status: Passed - CodeTree

--- 241008/최대로 연속한 숫자/maximum-consecutive-number.py
+++ 241008/최대로 연속한 숫자/maximum-consecutive-number.py
@@ -1,16 +1,18 @@
 from sortedcontainers import SortedSet
 
 n, m = map(int, input().split())
-removed = SortedSet([-1, n + 1])
-locs = {(-1, n + 1): n + 1}
+s_num = SortedSet([-1, n + 1])
+s_len = SortedSet([(-(n + 1), -1, n + 1)])
 
 for num in list(map(int, input().split())):
-    removed.add(num)
+    s_num.add(num)
 
-    idx = removed.bisect_left(num)
+    idx = s_num.bisect_left(num)
 
-    locs.pop((removed[idx - 1], removed[idx + 1]))
-    locs[(removed[idx - 1], removed[idx])] = removed[idx] - removed[idx - 1] - 1
-    locs[(removed[idx], removed[idx + 1])] = removed[idx + 1] - removed[idx] - 1
+    l, r = s_num[idx - 1], s_num[idx + 1]
+    m = s_num[idx]
+    s_len.remove((-(r - l - 1), l, r))
+    s_len.add((-(m - l - 1), l, m))
+    s_len.add((-(r - m - 1), m, r))
 
-    print(max(locs.values()))
+    print(-s_len[0][0])
