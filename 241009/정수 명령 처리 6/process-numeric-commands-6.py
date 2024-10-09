295530e24e36f633402306f8f4f082394f32c1a1
Se0 <54990890+onetuks@users.noreply.github.com>
2024-10-09 17:31:14 +0900

    Title: 정수 명령 처리 6, Time: 149ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 241009/정수 명령 처리 6/process-numeric-commands-6.py
@@ -0,0 +1,21 @@
+from heapq import heappush, heappop
+
+n = int(input())
+pq = list()
+
+for _ in range(n):
+    line = input().split()
+
+    if line[0] == 'push':
+        heappush(pq, -int(line[1]))
+    elif line[0] == 'pop':
+        print(-heappop(pq))
+    elif line[0] == 'size':
+        print(len(pq))
+    elif line[0] == 'empty':
+        if not pq:
+            print(1)
+        else:
+            print(0)
+    elif line[0] == 'top':
+        print(pq[0])
