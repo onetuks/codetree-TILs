4060fcf36e51a77055276ec221e405a34e2a85d5
Se0 <54990890+onetuks@users.noreply.github.com>
2024-10-10 00:19:59 +0900

    Title: 배열 추출, Time: 522ms, Memory: 34MB, Status: Passed - CodeTree

---
+++ 241010/배열 추출/array-extraction.py
@@ -0,0 +1,16 @@
+from heapq import heappush, heappop
+
+n = int(input())
+
+q = list()
+
+for _ in range(n):
+    num = int(input())
+
+    if num == 0:
+        if not q:
+            print(0)
+            continue
+        print(-heappop(q))
+    else:
+        heappush(q, -num)
