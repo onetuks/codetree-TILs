25e8d3d4baad4575a0aa81a0335ac7a299159ad0
Se0 <54990890+onetuks@users.noreply.github.com>
2024-10-10 01:18:23 +0900

    Title: 중앙값, Time: 699ms, Memory: 36MB, Status: Passed - CodeTree

--- 241010/중앙값/median.py
+++ 241010/중앙값/median.py
@@ -4,14 +4,31 @@ t = int(input())
 
 for _ in range(t):
     m = int(input())
-    q = list()
-    for idx, num in enumerate(list(map(int, input().split()))):
-        heappush(q, num)
-        if (idx + 1) % 2 != 0:
-            temp = list()
-            for _ in range((idx + 1) // 2):
-                temp.append(heappop(q))
-            print(q[0], end=" ")
-            for t in temp:
-                heappush(q, t)
+    arr = list(map(int, input().split()))
+    
+    median = arr[0]
+    maxq, minq = [], []
+    print(median, end=" ")
+
+    for i in range(1, m):
+        if i % 2 == 1: # 짝수 인덱스
+            if arr[i] < median:
+                heappush(maxq, -arr[i])
+            else:
+                heappush(minq, arr[i])
+
+        else:
+            candi = median
+            if len(maxq) < len(minq):
+                candi = heappop(minq)
+            else:
+                candi = -heappop(maxq)
+
+            nums = sorted([median, candi, arr[i]])
+
+            heappush(maxq, -nums[0])
+            median = nums[1]
+            heappush(minq, nums[-1])
+
+            print(median, end=" ")
     print()
