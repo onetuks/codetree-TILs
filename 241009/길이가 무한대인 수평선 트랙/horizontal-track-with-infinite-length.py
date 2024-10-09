a15331cca9fcaabaf393c97cc27096c4c999f464
Se0 <54990890+onetuks@users.noreply.github.com>
2024-10-09 17:18:06 +0900

    Title: 길이가 무한대인 수평선 트랙, Time: 447ms, Memory: 31MB, Status: Passed - CodeTree

--- 241009/길이가 무한대인 수평선 트랙/horizontal-track-with-infinite-length.py
+++ 241009/길이가 무한대인 수평선 트랙/horizontal-track-with-infinite-length.py
@@ -1,46 +1,12 @@
 from sortedcontainers import SortedSet
 
-def add_event(x1, v1, x2, v2):
-    if v1 <= v2:
-        return
-    events.add(((x2 - x1) / (v1 - v2), x1, v1))
-
-def remove_event(x1, v1, x2, v2):
-    if v1 <= v2:
-        return
-    events.remove(((x2 - x1) / (v1 - v2), x1, v1))
-
 n, t = map(int, input().split())
-x, v = list(), list()
-for _ in range(n):
-    ix, iv = map(int, input().split())
-    x.append(ix)
-    v.append(iv)
+tracks = [tuple(map(int, input().split())) for _ in range(n)]
 
-people = SortedSet()
-events = SortedSet()
+es = [s + v * t for s, v in tracks]
 
-for i in range(n):
-    people.add((x[i], v[i]))
-
-for i in range(n - 1):
-    add_event(x[i], v[i], x[i + 1], v[i + 1])
-
-while events:
-    ct, cx, cv = events[0]
-
-    if ct > t:
-        break
-
-    people.remove((cx, cv))
-    events.remove((ct, cx, cv))
-
-    idx = people.bisect_left((cx, cv))
-    nx, nv = people[idx]
-
-    if idx > 0:
-        px, pv = people[idx - 1]
-        remove_event(px, pv, cx, cv)
-        add_event(px, pv, nx, nv)
-
-print(len(people))
+for i in reversed(range(1, len(es))):
+    if es[i - 1] >= es[i]:
+        es[i - 1] = es[i]
+    
+print(len(SortedSet(es)))
