628c78cb2ebdedf3f59c5c61585a5c5b01fa8cb3
Se0 <54990890+onetuks@users.noreply.github.com>
2024-10-11 16:37:59 +0900

    Title: 책꽂이 정리, Time: 812ms, Memory: 66MB, Status: Passed - CodeTree

--- 241011/책꽂이 정리/bookshelf-clean.py
+++ 241011/책꽂이 정리/bookshelf-clean.py
@@ -1,103 +1,129 @@
 class Node:
-    def __init__(self, num):
-        self.num = num
+    def __init__(self, id):
+        self.id = id
         self.prev = None
         self.post = None
 
-    def __str__(self):
-        prev_num = self.prev.num if self.prev is not None else 0
-        post_num = self.post.num if self.post is not None else 0
-        return "%d %d %d" %(prev_num, self.num, post_num)
-
-class LinkedList:
-    def __init__(self):
-        self.head = None
-        self.tail = None
-
-    def __str__(self):
-        nums = []
-        node = self.head 
-        while node is not None:
-            nums.append(node.num)
-            node = node.post
-        return str(len(nums)) + " " + ' '.join(map(str, nums))
-
-    def insert_head(self, node):
-        if node is None:
-            return
-        if self.head is None:
-            self.head = node
-            self.tail = node
-        else:
-            connect(node, self.head)
-            self.head = node
-
-    def insert_tail(self, node):
-        if node is None:
-            return
-        if self.tail is None:
-            self.head = node
-            self.tail = node
-        else:
-            connect(self.tail, node)
-            self.tail = node
-
-    def pop_head(self):
-        if self.head is None:
-            return None
-        head = self.head
-        self.head = head.post
-        disconnect(head, head.post)
-        return head
-
-    def pop_tail(self):
-        if self.tail is None:
-            return None
-        tail = self.tail
-        self.tail = tail.prev
-        disconnect(tail.prev, tail)
-        return tail
+def is_empty(num):
+    return heads[num] == 0
 
 def connect(s, e):
-    if s is not None:
+    if s:
         s.post = e
-    if e is not None:
+    if e:
         e.prev = s
 
-def disconnect(s, e):
-    if s is not None:
-        s.post = None
-    if e is not None:
-        e.prev = None
+def pop_front(num):
+    node = nodes[heads[num]] 
+    if not node:
+        return None
+
+    if node.post:
+        heads[num] = node.post.id
+        node.post.prev = None
+    else:
+        heads[num] = 0
+        tails[num] = 0
+
+    node.post = None
+    
+    return node
+
+def pop_back(num):
+    node = nodes[tails[num]]
+    if not node:
+        return None
+
+    if node.prev:
+        tails[num] = node.prev.id
+        node.prev.post = None
+    else:
+        heads[num] = 0
+        tails[num] = 0
+
+    node.prev = None
+
+    return node
+
+def push_front(num, node):
+    head = nodes[heads[num]]
+    if head:
+        connect(node, head)
+        heads[num] = node.id
+    else:
+        heads[num] = node.id
+        tails[num] = node.id
+
+def push_back(num, node):
+    tail = nodes[tails[num]]
+    if tail:
+        connect(tail, node)
+        tails[num] = node.id
+    else:
+        heads[num] = node.id
+        tails[num] = node.id
+
+def move_all_front(i, j):
+    if i == j or is_empty(i):
+        return
+    
+    if is_empty(j):
+        heads[j] = heads[i]
+        tails[j] = tails[i]
+    else:
+        connect(nodes[tails[i]], nodes[heads[j]])
+        heads[j] = heads[i] 
+    
+    heads[i] = tails[i] = 0
+
+def move_all_back(i, j):
+    if i == j or is_empty(i):
+        return
+
+    if is_empty(j):
+        heads[j] = heads[i]
+        tails[j] = tails[i]
+    else:
+        connect(nodes[tails[j]], nodes[heads[i]])
+        tails[j] = tails[i]
+    
+    heads[i] = tails[i] = 0
 
 n, k = map(int, input().split())
 q = int(input())
 
-shelves = [LinkedList() for _ in range(k + 1)]
-for i in range(1, n + 1):
-    shelves[1].insert_tail(Node(i))
+heads = [0] * (k + 1)
+tails = [0] * (k + 1)
+
+nodes = [None] + [Node(i) for i in range(1, n + 1)]
+
+for i in range(1, n):
+    connect(nodes[i], nodes[i + 1])
+
+heads[1] = 1
+tails[1] = n
 
 for _ in range(q):
     x, i, j = map(int, input().split())
 
     if x == 1:
-        node = shelves[i].pop_head()
-        shelves[j].insert_tail(node)
+        target = pop_front(i)
+        if target:
+            push_back(j, target)
     elif x == 2:
-        node = shelves[i].pop_tail()
-        shelves[j].insert_head(node)
+        target = pop_back(i)
+        if target:
+            push_front(j, target)
     elif x == 3:
-        shelves[j].insert_head(shelves[i].tail)
-        shelves[j].head = shelves[i].head
-        if i != j:
-            shelves[i].head = None
-            shelves[i].tail = None
+        move_all_front(i, j)
     elif x == 4:
-        shelves[j].insert_tail(shelves[i].head)
-        shelves[j].tail = shelves[i].tail
-        if i != j:
-            shelves[i].head = None
-            shelves[i].tail = None
+        move_all_back(i, j)
 
-for shelf in shelves[1:]:
-    print(shelf)
+for i in range(1, k + 1):
+    ids = []
+    node = nodes[heads[i]]
+    while node:
+        ids.append(node.id)
+        node = node.post
+
+    print(len(ids), ' '.join(map(str, ids)))
