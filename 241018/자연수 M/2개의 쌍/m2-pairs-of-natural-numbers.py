6d5c2bcbf9147127ef072e8d4edb0fd87dc51c6e
Se0 <54990890+onetuks@users.noreply.github.com>
2024-10-18 00:59:36 +0900

    Title: 자연수 M/2개의 쌍, Time: 133ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 241018/자연수 M/2개의 쌍/m2-pairs-of-natural-numbers.py
@@ -0,0 +1,24 @@
+n = int(input())
+# (개수, 숫자)
+nums = [tuple(map(int, input().split())) for _ in range(n)]
+nums.sort(key=lambda x: x[1])
+
+ans = 1e11
+i, j = 0, n - 1
+while i <= j:
+    ci, ni = nums[i]
+    cj, nj = nums[j]
+
+    ans = min(ans, ni + nj)
+
+    if ci < cj:
+        nums[j] = (cj - ci, nj)
+        j -= 1
+    elif ci > cj:
+        nums[i] = (ci - cj, ni)
+        i += 1
+    else:
+        i += 1
+        j -= 1
+
+print(ans)
