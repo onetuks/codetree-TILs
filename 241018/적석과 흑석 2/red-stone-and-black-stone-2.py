99eecbd38e9ee0b7794885fe1162193e846cb3bb
Se0 <54990890+onetuks@users.noreply.github.com>
2024-10-18 01:09:46 +0900

    Title: 적석과 흑석 2, Time: 1038ms, Memory: 40MB, Status: Passed - CodeTree

--- 241018/적석과 흑석 2/red-stone-and-black-stone-2.py
+++ 241018/적석과 흑석 2/red-stone-and-black-stone-2.py
@@ -4,6 +4,8 @@ c, n = map(int, input().split())
 reds = SortedSet(int(input()) for _ in range(c))
 blacks = [tuple(map(int, input().split())) for _ in range(n)]
 
+blacks.sort(key=lambda x: x[1])
+
 ans = 0
 
 for a, b in blacks:
