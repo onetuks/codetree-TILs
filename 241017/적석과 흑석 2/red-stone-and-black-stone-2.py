e1827dbe78c3913b5a3b77a4b0ba2506dadb20c5
Se0 <54990890+onetuks@users.noreply.github.com>
2024-10-17 16:40:57 +0900

    Title: 적석과 흑석 2, Time: 395ms, Memory: 27MB, Status: Wrong Answer - CodeTree

--- 241017/적석과 흑석 2/red-stone-and-black-stone-2.py
+++ 241017/적석과 흑석 2/red-stone-and-black-stone-2.py
@@ -2,13 +2,13 @@ c, n = map(int, input().split())
 reds = [int(input()) for _ in range(c)]
 blacks = [tuple(map(int, input().split())) for _ in range(n)]
 
-reds.sort()
-blacks.sort(key=lambda x: (x[1], x[0]))
+reds.sort(reverse=True)
+blacks.sort(key=lambda x: (-x[1], -x[0]))
 
 ans = 0
 idx = 0
 for a, b in blacks:
-    while idx < c and reds[idx] < a:
+    while idx < c and reds[idx] > b:
         idx += 1
     
     if idx < c and a <= reds[idx] <= b:
