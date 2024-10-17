eefb8b2f9d106474a272752d44930ecd8c2c3f9f
Se0 <54990890+onetuks@users.noreply.github.com>
2024-10-17 16:28:02 +0900

    Title: 자연수 M/2개의 쌍, Time: 395ms, Memory: 34MB, Status: Passed - CodeTree

--- 241017/자연수 M/2개의 쌍/m2-pairs-of-natural-numbers.py
+++ 241017/자연수 M/2개의 쌍/m2-pairs-of-natural-numbers.py
@@ -7,17 +7,19 @@ for _ in range(n):
 ckeys = sorted(list(cdic.keys()))
 
 i, j = 0, n - 1
-ans = 1e11
+ans = 0
 
 while i <= j or cdic[ckeys[i]] > 0 or cdic[ckeys[j]] > 0:
-    ans = min(ans, ckeys[i] + ckeys[j])
+    ans = max(ans, ckeys[i] + ckeys[j])
 
-    cdic[ckeys[i]] -= min(cdic[ckeys[i]], cdic[ckeys[j]])
-    cdic[ckeys[j]] -= min(cdic[ckeys[i]], cdic[ckeys[j]])
+    cnt = min(cdic[ckeys[i]], cdic[ckeys[j]])
 
-    if cdic[ckeys[i]] == 0:
+    cdic[ckeys[i]] -= cnt
+    cdic[ckeys[j]] -= cnt
+
+    if cdic[ckeys[i]] <= 0:
         i += 1
-    if cdic[ckeys[j]] == 0:
+    if cdic[ckeys[j]] <= 0:
         j -= 1
 
 print(ans)
