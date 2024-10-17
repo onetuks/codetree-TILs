7c26b40f39fd3e6d027878669a945658c012a8a3
Se0 <54990890+onetuks@users.noreply.github.com>
2024-10-17 16:22:59 +0900

    Title: 자연수 M/2개의 쌍, Time: 100ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 241017/자연수 M/2개의 쌍/m2-pairs-of-natural-numbers.py
+++ 241017/자연수 M/2개의 쌍/m2-pairs-of-natural-numbers.py
@@ -12,8 +12,8 @@ ans = 1e11
 while i <= j or cdic[ckeys[i]] > 0 or cdic[ckeys[j]] > 0:
     ans = min(ans, ckeys[i] + ckeys[j])
 
-    cdic[ckeys[i]] -= 1
-    cdic[ckeys[j]] -= 1
+    cdic[ckeys[i]] -= min(cdic[ckeys[i]], cdic[ckeys[j]])
+    cdic[ckeys[j]] -= min(cdic[ckeys[i]], cdic[ckeys[j]])
 
     if cdic[ckeys[i]] == 0:
         i += 1
