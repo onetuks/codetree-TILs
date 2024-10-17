30450a4ad5c5be64fb9ddc761ce6059bcd094741
Se0 <54990890+onetuks@users.noreply.github.com>
2024-10-18 00:43:00 +0900

    Title: 수 채우기, Time: 114ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 241018/수 채우기/fill-in-number.py
@@ -0,0 +1,11 @@
+n = int(input())
+m = 100000
+
+ans = 1e11
+
+for i in range(m // 5 + 1):
+    rest = n - 5 * i
+    if rest >= 0 and rest % 2 == 0:
+        ans = min(ans, i + rest // 2)
+
+print(ans)
