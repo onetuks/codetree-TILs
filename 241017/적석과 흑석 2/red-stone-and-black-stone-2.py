1c28e04cf3e63767154497e1903519ef5a05c582
Se0 <54990890+onetuks@users.noreply.github.com>
2024-10-17 16:57:05 +0900

    Title: 적석과 흑석 2, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 241017/적석과 흑석 2/red-stone-and-black-stone-2.py
+++ 241017/적석과 흑석 2/red-stone-and-black-stone-2.py
@@ -1,18 +1,16 @@
+from sortedcontainers import SortedSet
+
 c, n = map(int, input().split())
-reds = [int(input()) for _ in range(c)]
+reds = SortedSet(int(input()) for _ in range(c))
 blacks = [tuple(map(int, input().split())) for _ in range(n)]
 
-reds.sort()
 blacks.sort(key=lambda x: (x[1]))
 
 ans = 0
-idx = 0
 for a, b in blacks:
-    while idx < c and reds[idx] < a:
-        idx += 1
-    
-    if idx < c and a <= reds[idx] <= b:
+    idx = reds.bisect_left(a)
+    if reds[idx] <= b:
+        reds.pop(idx)
         ans += 1
-        idx += 1
 
 print(ans)
