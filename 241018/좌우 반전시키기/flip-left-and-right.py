ce001992f273d05d904517704f10d4b567159721
Se0 <54990890+onetuks@users.noreply.github.com>
2024-10-18 17:58:45 +0900

    Title: 좌우 반전시키기, Time: 118ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 241018/좌우 반전시키기/flip-left-and-right.py
+++ 241018/좌우 반전시키기/flip-left-and-right.py
@@ -10,7 +10,7 @@ def convert(i):
         nums[i + 1] = (nums[i + 1] + 1) % 2
 
 for i in range(1, n):
-    if nums[i - 1] == 0 and nums[i] == 0:
+    if nums[i - 1] == 0:
         convert(i)
         ans += 1
 
