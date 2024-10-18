c25f1209fe1c37069a20749fa6860514368b5407
Se0 <54990890+onetuks@users.noreply.github.com>
2024-10-18 18:32:52 +0900

    Title: 상하좌우 반전시키기, Time: 142ms, Memory: 25MB, Status: Passed - CodeTree

--- 241018/상하좌우 반전시키기/flip-up-down-left-right.py
+++ 241018/상하좌우 반전시키기/flip-up-down-left-right.py
@@ -1,5 +1,4 @@
 dlist = [(0, 1), (0, -1), (1, 0), (-1, 0)]
-slist = [(0, -1), (-1, 0)]
 
 def convert(i, j):
     matrix[i][j] ^= 1
@@ -13,16 +12,9 @@ matrix = [list(map(int, input().split())) for _ in range(n)]
 
 ans = 0
 
-for i in range(n):
+for i in range(1, n):
     for j in range(n):
-        if i == 0 and j == 0:
-            continue
-        zcnt = matrix[i][j] ^ 1
-        for sx, sy in slist:
-            si, sj = i + sx, j + sy
-            if 0 <= si < n and 0 <= sj < n and matrix[si][sj] == 0:
-                zcnt += 1
-        if zcnt >= 2:
+        if matrix[i - 1][j] == 0:
             ans += 1
             convert(i, j)
 
