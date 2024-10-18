464a4e4ec76c49bd8f206bd924bf302e7c550255
Se0 <54990890+onetuks@users.noreply.github.com>
2024-10-18 22:07:38 +0900

    Title: 다른 경로로 이동, Time: 118ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 241018/다른 경로로 이동/move-with-different-path.py
+++ 241018/다른 경로로 이동/move-with-different-path.py
@@ -13,9 +13,12 @@ def dijkstra():
             continue
 
         for next_node in range(1, n + 1):
-            if dp[next_node] >= dp[node] + adj[node][next_node]:
-                dp[next_node] = dp[node] + adj[node][next_node]
+            next_cost = dp[node] + adj[node][next_node]
+            if dp[next_node] > next_cost:
+                dp[next_node] = next_cost
                 heappush(q, (dp[next_node], next_node))
+                path[next_node] = node
+            elif dp[next_node] == next_cost and path[next_node] > node:
                 path[next_node] = node
 
     return dp[n]
