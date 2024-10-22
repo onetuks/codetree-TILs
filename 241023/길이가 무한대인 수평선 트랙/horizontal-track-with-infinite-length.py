ee3395316d46439075c5bd5a965b49edd7dd71e8
Se0 <54990890+onetuks@users.noreply.github.com>
2024-10-23 01:40:05 +0900

    Title: 길이가 무한대인 수평선 트랙, Time: 273ms, Memory: 26MB, Status: Wrong Answer - CodeTree

---
+++ 241023/길이가 무한대인 수평선 트랙/horizontal-track-with-infinite-length.py
@@ -0,0 +1,19 @@
+from sortedcontainers import SortedSet
+
+n, t = map(int, input().split())
+mans = SortedSet()
+
+for _ in range(n):
+    s, v = map(int, input().split())
+    mans.add((s, v))
+
+ans = 1
+
+for i in range(1, n):
+    s1, v1 = mans[i - 1]
+    s2, v2 = mans[i]
+    if v1 > v2 and s1 + v1 * t >= s2 + v2 * t:
+        continue
+    ans += 1
+    
+print(ans)
