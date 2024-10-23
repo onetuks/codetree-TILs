ed334644c7c4e857a54341bcd6580fe1044132cb
Se0 <54990890+onetuks@users.noreply.github.com>
2024-10-23 23:21:52 +0900

    Title: 폭탄 해체 작업, Time: 623ms, Memory: 25MB, Status: Passed - CodeTree

--- 241023/폭탄 해체 작업/the-bomb-dismantling.java
+++ 241023/폭탄 해체 작업/the-bomb-dismantling.java
@@ -1,40 +1,52 @@
 import java.util.*;
 
+class Bomb implements Comparable<Bomb> {
+    int timeLimit;
+    int score;
+
+    Bomb(int timeLimit, int score) {
+        this.timeLimit = timeLimit;
+        this.score = score;
+    }
+
+    @Override
+    public int compareTo(Bomb o) {
+        if (this.timeLimit == o.timeLimit) 
+            return this.score - o.score;
+        return this.timeLimit - o.timeLimit;
+    }
+}
+
 public class Main {
     
     private static Scanner sc = new Scanner(System.in);
     private static int n;
-    private static List<int[]> bombs = new ArrayList<>();
+    private static List<Bomb> bombs = new ArrayList<>();
 
     public static void main(String[] args) {
-        n = sc.nextInt();
-
-        for (int i = 0; i < n; i ++) {
-            int s = sc.nextInt();
-            int l = sc.nextInt();
-            bombs.add(new int[]{l, s});
-        }
-
-        Collections.sort(bombs, (a, b) -> {
-            if (a[0] == b[0]) {
-                return a[1] - b[1];
-            }
-            return a[0] - b[0];
-        });
-
-        int idx = n - 1;
-        PriorityQueue<Integer> scores = new PriorityQueue<>((a, b) -> b - a);
-
         int ans = 0;
 
-        for (int time = bombs.get(idx)[0]; time > 0; time--) {
-            while (idx >= 0 && time <= bombs.get(idx)[0]) {
-                scores.add(bombs.get(idx)[1]);
+        n = sc.nextInt();
+
+        for (int i = 0; i < n; i++) {
+            int score = sc.nextInt();
+            int timeLimit = sc.nextInt();
+            bombs.add(new Bomb(timeLimit, score));
+        }
+
+        Collections.sort(bombs);
+
+        int idx = n - 1;
+        PriorityQueue<Integer> pq = new PriorityQueue<>((a, b) -> b - a);
+
+        for (int time = bombs.get(idx).timeLimit; time > 0; time--) {
+            while (idx >= 0 && time <= bombs.get(idx).timeLimit) {
+                pq.add(bombs.get(idx).score);
                 idx--;
             }
 
-            if (!scores.isEmpty()) {
-                ans += scores.poll();
+            if (!pq.isEmpty()) {
+                ans += pq.poll();
             }
         }
 
