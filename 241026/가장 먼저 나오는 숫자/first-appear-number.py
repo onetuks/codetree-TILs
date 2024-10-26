4ea01f2623092c8181292b8aa965e4bd2f2459f6
Se0 <54990890+onetuks@users.noreply.github.com>
2024-10-26 20:07:13 +0900

    Title: 가장 먼저 나오는 숫자, Time: 236ms, Memory: 34MB, Status: Passed - CodeTree

--- 241026/가장 먼저 나오는 숫자/first-appear-number.py
+++ 241026/가장 먼저 나오는 숫자/first-appear-number.py
@@ -1,22 +1,29 @@
-n, m = map(int, input().split())
-arr = list(map(int, input().split()))
+# 변수 선언 및 입력:
+n, m = tuple(map(int, input().split()))
+arr = [0] + list(map(int, input().split()))
+querries = list(map(int, input().split()))
 
-def lower_bound(x):
-    l, r = 0, n - 1
-    min_idx = n
-    while l <= r:
-        m = (l + r) // 2
-        if x == arr[m]:
-            min_idx = min(min_idx, m)
-        if x <= arr[m]:
-            r = m - 1
+# target보다 같거나 큰 최초의 위치를 반환합니다.
+def lower_bound(target):
+    left, right = 1, n
+    min_idx = n + 1
+    while left <= right:
+        mid = (left + right) // 2
+        if arr[mid] >= target:
+            right = mid - 1
+            min_idx = min(min_idx, mid)
         else:
-            l = m + 1
+            left = mid + 1
+
     return min_idx
 
-for x in list(map(int, input().split())):
-    ans = lower_bound(x)
-    if 0 <= ans < n and arr[ans] == x:
-        print(ans + 1)
+for querry in querries:
+    # 이진탐색을 진행합니다.
+    # querry보다 크거나 같은 최소의 값의 위치를 구하고
+    # 그 값이 querry와 같으면 위치를,
+    # 다르면 -1을 출력합니다.
+    lo = lower_bound(querry)
+    if lo <= n and arr[lo] == querry:
+        print(lo)
     else:
         print(-1)
