8496ba61a6529f9713065136cea299dadb080b6e
Se0 <54990890+onetuks@users.noreply.github.com>
2024-10-26 20:00:51 +0900

    Title: 숫자의 개수, Time: 506ms, Memory: 33MB, Status: Passed - CodeTree

--- 241026/숫자의 개수/number-of-integers.py
+++ 241026/숫자의 개수/number-of-integers.py
@@ -1,10 +1,32 @@
-from bisect import bisect_left, bisect_right
-
 n, m = map(int, input().split())
 arr = list(map(int, input().split()))
 
+def lower_bound(x):
+    l, r = 0, n - 1
+    min_idx = n
+    while l <= r:
+        m = (l + r) // 2
+        if x <= arr[m]:
+            min_idx = min(min_idx, m)
+            r = m - 1
+        else:
+            l = m + 1
+    return min_idx
+
+def upper_bound(x):
+    l, r = 0, n - 1
+    min_idx = n
+    while l <= r:
+        m = (l + r) // 2
+        if x < arr[m]:
+            min_idx = min(min_idx, m)
+            r = m - 1
+        else:
+            l = m + 1
+    return min_idx
+
 for _ in range(m):
     x = int(input())
-    lower = bisect_left(arr, x)
-    upper = bisect_right(arr, x)
+    lower = lower_bound(x)
+    upper = upper_bound(x)
     print(upper - lower)
