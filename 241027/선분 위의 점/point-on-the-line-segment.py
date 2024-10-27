d73d0660db49f8e3020ea5c26e82bee2619ba8e0
Se0 <54990890+onetuks@users.noreply.github.com>
2024-10-27 16:16:38 +0900

    Title: 선분 위의 점, Time: 224ms, Memory: 26MB, Status: Wrong Answer - CodeTree

--- 241027/선분 위의 점/point-on-the-line-segment.py
+++ 241027/선분 위의 점/point-on-the-line-segment.py
@@ -13,9 +13,22 @@ def lower_bound(x):
             l = m + 1
     return min_idx
 
+def upper_bound(x):
+    l, r = 0, n - 1
+    min_idx = n
+    while l <= r:
+        m = (l + r) // 2
+        if x < dots[m]:
+            min_idx = min(min_idx, m)
+            r = m - 1
+        else:
+            l = m + 1
+    return min_idx
+
+
 for _ in range(m):
     s, e = map(int, input().split())
     s, e = min(s, e), max(s, e)
     s_min_idx = lower_bound(s)
-    e_min_idx = lower_bound(e)
+    e_min_idx = upper_bound(e)
     print(e_min_idx - s_min_idx)
