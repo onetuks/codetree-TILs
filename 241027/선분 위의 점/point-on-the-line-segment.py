dc3b49b065266ce008bf1c90bb7f3f61be655685
Se0 <54990890+onetuks@users.noreply.github.com>
2024-10-27 16:35:01 +0900

    Title: 선분 위의 점, Time: 106ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 241027/선분 위의 점/point-on-the-line-segment.py
+++ 241027/선분 위의 점/point-on-the-line-segment.py
@@ -1,24 +1,35 @@
 n, m = map(int, input().split())
 dots = list(map(int, input().split()))
 
-def upper_bound(x):
+def lower_bound(x):
     l, r = 0, n - 1
-    max_idx = -1
+    min_idx = n
     while l <= r:
         m = (l + r) // 2
-        if x >= dots[m]:
-            max_idx = max(max_idx, m)
-            l = m + 1
-        else:
+        if dots[m] >= x:
+            min_idx = min(min_idx, m)
             r = m - 1
-    return max_idx
+        else:
+            l = m + 1
+    return min_idx
+
+def upper_bound(x):
+    l, r = 0, n - 1
+    min_idx = n
+    while l <= r:
+        m = (l + r) // 2
+        if dots[m] > x:
+            min_idx = min(min_idx, m)
+            r = m - 1
+        else:
+            l = m + 1
+    return min_idx
 
 for _ in range(m):
     s, e = map(int, input().split())
     s, e = min(s, e), max(s, e)
     s_min_idx = upper_bound(s)
     e_min_idx = upper_bound(e)
-    ans = e_min_idx - s_min_idx
-    if dots[s_min_idx] == s and dots[e_min_idx] == e:
-        ans += 1
-    print(ans)
+    if s_min_idx == 0 and dots[s_min_idx] < s:
+        s_min_idx -= 1
+    print(e_min_idx - s_min_idx)
