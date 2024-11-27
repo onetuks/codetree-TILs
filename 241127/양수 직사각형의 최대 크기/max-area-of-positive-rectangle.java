2afae88e1b25f4e2d3d971c9fab45ab25b66ca0e
Se0 <54990890+onetuks@users.noreply.github.com>
2024-11-27 16:17:19 +0900

    Title: 양수 직사각형의 최대 크기, Time: 83ms, Memory: 9MB, Status: Wrong Answer - CodeTree

---
+++ 241127/양수 직사각형의 최대 크기/max-area-of-positive-rectangle.java
@@ -0,0 +1,52 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+
+    private static int n;
+    private static int m;
+    private static int[][] matrix;
+
+    private static int answer = -1;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+
+        matrix = new int[n][m];
+        for (int i = 0; i < n; i ++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < m; j++)
+                matrix[i][j] = Integer.parseInt(st.nextToken());
+        }
+
+        for (int i = 0; i < n; i ++) {
+            for (int j = 0; j < m; j++) {
+                for (int k = i; k < n; k ++) {
+                    for (int l = j; l < m; l ++) {
+                        if (hasAllPositive(i, j, k, l)) {
+                            answer = Math.max(answer, (k - i + 1) * (l - j + 1));
+                        }
+                    }
+                }
+            }
+        }
+
+        System.out.println(answer);
+    }
+
+    private static boolean hasAllPositive(int i, int j, int k, int l) {
+        for (int u = i; u <= k; u ++) {
+            for (int v = j; v <= l; v ++) {
+                if (matrix[u][v] < 0) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+}
