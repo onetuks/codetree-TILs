ccec3b07c0dad029058e46fcc3d978d8117d807c
Se0 <54990890+onetuks@users.noreply.github.com>
2024-11-27 15:21:12 +0900

    Title: 기울어진 직사각형, Time: 111ms, Memory: 10MB, Status: Passed - CodeTree

--- 241127/기울어진 직사각형/slanted-rectangle.java
+++ 241127/기울어진 직사각형/slanted-rectangle.java
@@ -3,8 +3,10 @@ import java.io.*;
 
 public class Main {
 
+    private static final int[][] dlist = {{-1, +1}, {-1, -1}, {+1, -1}, {+1, +1}};
+
     private static int n;
-    private static int[][] matrix;
+    private static int[][] matrix; 
 
     private static int answer = 0;
 
@@ -18,50 +20,36 @@ public class Main {
         matrix = new int[n][n];
         for (int i = 0; i < n; i ++) {
             st = new StringTokenizer(br.readLine());
-            for (int j = 0 ; j < n; j ++) {
+            for (int j = 0; j < n; j ++) {
                 matrix[i][j] = Integer.parseInt(st.nextToken());
             }
         }
 
-        for (int i = 2; i < n; i ++) {
-            for (int j = 1; j < n - 1; j ++) {
-                int sum = getSum(i, j);
-                answer = Math.max(answer, sum);
-            }
-        }
+        for (int i = 0; i < n; i ++)
+            for (int j = 0; j < n; j ++)
+                for (int k = 1; k < n; k ++)
+                    for (int l = 1; l < n; l ++)
+                        answer = Math.max(answer, getSum(i, j, k, l));
+
 
         System.out.println(answer);
     }
 
-    private static int getSum(int x, int y) {
-        int i = x, j = y;
+    private static int getSum(int i, int j, int k, int l) {
+        int[] moveCount = {k, l, k, l};
+
         int sum = 0;
-        
-        while (i - 1 > 0 && j + 1 < n) {
-            i = i - 1;
-            j = j + 1;
-            sum += matrix[i][j];
-        }
 
-        while (i - 1 >= 0 && j - 1 > 0) {
-            i = i - 1;
-            j = j - 1;
-            sum += matrix[i][j];
-        }
+        for (int d = 0; d < dlist.length; d ++) {
+            for (int cnt = 0; cnt < moveCount[d]; cnt ++) {
+                i += dlist[d][0];
+                j += dlist[d][1];
 
-        while (i + 1 < n - 1 && j - 1 >= 0) {
-            i = i + 1;
-            j = j - 1;
-            sum += matrix[i][j];
-        }
+                if (i < 0 || i >= n || j < 0 || j >= n)
+                    return 0;
 
-        while (i + 1 < n && j + 1 < n - 1) {
-            i = i + 1;
-            j = j + 1;
-            sum += matrix[i][j];
-
-            if (i == x && j == y)
-                break;
+                sum += matrix[i][j];
+            }
         }
 
         return sum;
