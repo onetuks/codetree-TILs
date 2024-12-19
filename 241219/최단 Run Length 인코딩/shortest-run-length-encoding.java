3d530dd969a62e0b3fe372e7bb5d10181f36d1b5
Se0 <54990890+onetuks@users.noreply.github.com>
2024-12-19 17:31:18 +0900

    Title: 최단 Run Length 인코딩, Time: 125ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 241219/최단 Run Length 인코딩/shortest-run-length-encoding.java
@@ -0,0 +1,50 @@
+import java.util.*;
+
+public class Main {
+
+    private static int answer = Integer.MAX_VALUE;
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        char[] word = sc.nextLine().toCharArray();
+
+        for (int i = 1; i < word.length; i ++) {
+            word = shift(word);
+            answer = Math.min(answer, getEncodedLength(word));
+        }
+
+        System.out.println(answer);
+    }
+
+    private static char[] shift(char[] word) {
+        char[] shiftedWord = new char[word.length];
+
+        for (int i = 1; i < word.length; i ++) {
+            shiftedWord[i] = word[i - 1];
+        }
+        shiftedWord[0] = word[word.length - 1];
+        return shiftedWord;
+    }
+
+    private static int getEncodedLength(char[] word) {
+        StringBuilder sb = new StringBuilder();
+        
+        int cnt = 0;
+        int chr = word[0];
+        for (char w: word) {
+            if (chr == w) {
+                cnt++;
+            } else {
+                sb.append(chr + cnt);
+                cnt = 0;
+                chr = w;
+            }
+        }
+        if (cnt > 0) {
+            sb.append(chr + cnt);
+        }
+
+        return sb.length();
+    }
+}
