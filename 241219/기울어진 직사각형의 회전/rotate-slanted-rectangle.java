c9f588d051acd27278f4b8243b0430bc080e3043
Se0 <54990890+onetuks@users.noreply.github.com>
2024-12-19 17:15:28 +0900

    Title: 기울어진 직사각형의 회전, Time: 271ms, Memory: 16MB, Status: Passed - CodeTree

--- 241219/기울어진 직사각형의 회전/rotate-slanted-rectangle.java
+++ 241219/기울어진 직사각형의 회전/rotate-slanted-rectangle.java
@@ -1,82 +1,77 @@
-import java.util.*;
-import java.io.*;
+import java.util.Scanner;
+import java.util.Arrays;
+import java.util.ArrayList;
 
 public class Main {
-
-    private static final int m = 4;
-    private static final int[][] COUNTER_CLOCKWISE_DLIST = {{-1, -1}, {-1, 1}, {1, 1}, {1, -1}};
-    private static final int[][] CLOCKWISE_DLIST = {{-1, 1}, {-1, -1}, {1, -1}, {1, 1}};
-
-    private static BufferedReader br;
-    private static StringTokenizer st;
-
-    private static int n;
-    private static int[][] matrix;
-
-    public static void main(String[] args) throws IOException {
-        setup();
-
-        st = new StringTokenizer(br.readLine());
-
-        int r = Integer.parseInt(st.nextToken()) - 1;
-        int c = Integer.parseInt(st.nextToken()) - 1;
-
-        int[] mlist = new int[m];
-        for (int i = 0; i < m; i ++) {
-            mlist[i] = Integer.parseInt(st.nextToken());
+    public static final int CW = 1;
+    public static final int CCW = 0;
+    public static final int DIR_NUM = 4;
+    public static final int MAX_N = 100;
+    
+    public static int n;
+    public static int[][] grid = new int[MAX_N][MAX_N];
+    public static int[][] temp = new int[MAX_N][MAX_N];
+    
+    public static void shift(int x, int y, int k, int l, int moveDir) {
+        int[] dx;
+        int[] dy;
+        int[] moveNums;
+        
+        if(moveDir == CCW) {
+            dx = new int[]{-1, -1, 1, 1};
+            dy = new int[]{1, -1, -1, 1};
+            moveNums = new int[]{k, l, k, l}; 
         }
-
-        int dir = Integer.parseInt(st.nextToken());
-
-        rotate(r, c, mlist, dir);
-
-        print();
-    }
-
-    private static void rotate(int r, int c, int[] mlist, int dir) {
-        int temp = matrix[r][c];
-        int x = r, y = c;
-
-        for (int i = 0; i < m; i ++) {
-            int dist = dir == 0 ? mlist[m - 1 - i] : mlist[i];
-            while (dist-- > 0) {
-                int dx = x + (dir == 0 ? COUNTER_CLOCKWISE_DLIST[i][0] : CLOCKWISE_DLIST[i][0]);
-                int dy = y + (dir == 0 ? COUNTER_CLOCKWISE_DLIST[i][1] : CLOCKWISE_DLIST[i][1]);
-                matrix[x][y] = matrix[dx][dy];
-                x = dx;
-                y = dy;
-            }
+        else {
+            dx = new int[]{-1, -1, 1, 1};
+            dy = new int[]{-1, 1, 1, -1};
+            moveNums = new int[]{l, k, l, k};
         }
         
-        if (dir == 0) {
-            matrix[r - 1][c + 1] = temp;
-        } else {
-            matrix[r - 1][c - 1] = temp;
-        }
+        // Step1. temp 배열에 grid 값을 복사합니다.
+        for(int i = 0; i < n; i++)
+            for(int j = 0; j < n; j++)
+                temp[i][j] = grid[i][j];
+    
+        // Step2. 기울어진 직사각형의 경계를 쭉 따라가면서
+        //        숫자를 한 칸씩 밀었을 때의 결과를
+        //        temp에 저장합니다.
+        for(int d = 0; d < DIR_NUM; d++)
+            for(int q = 0; q < moveNums[d]; q++) {
+                int nx = x + dx[d], ny = y + dy[d];
+                temp[nx][ny] = grid[x][y];
+                x = nx; y = ny;
+            }
+        
+        // Step3. temp값을 grid에 옮겨줍니다.
+        for(int i = 0; i < n; i++)
+            for(int j = 0; j < n; j++)
+                grid[i][j] = temp[i][j];
     }
 
-    private static void print() {
-        for (int i = 0; i < n; i ++) {
-            for (int j = 0; j < n; j ++) {
-                System.out.print(matrix[i][j] + " ");
-            }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        n = sc.nextInt();
+        
+        for(int i = 0; i < n; i++)
+            for(int j = 0; j < n; j++) 
+                grid[i][j] = sc.nextInt();
+        
+        int x, y, m1, m2, m3, m4, d;
+        x = sc.nextInt();
+        y = sc.nextInt();
+        m1 = sc.nextInt();
+        m2 = sc.nextInt();
+        m3 = sc.nextInt();
+        m4 = sc.nextInt();
+        d = sc.nextInt();
+        
+        shift(x - 1, y - 1, m1, m2, d);
+        
+        for(int i = 0; i < n; i++) {
+            for(int j = 0; j < n; j++)
+                System.out.print(grid[i][j] + " ");
             System.out.println();
-        }
-    }
-
-    private static void setup() throws IOException {
-        br = new BufferedReader(new InputStreamReader(System.in));
-
-        st = new StringTokenizer(br.readLine());
-
-        n = Integer.parseInt(st.nextToken());
-
-        matrix = new int[n][n];
-        for (int i = 0; i < n; i ++) {
-            st = new StringTokenizer(br.readLine());
-            for (int j = 0; j < n; j ++) {
-                matrix[i][j] = Integer.parseInt(st.nextToken());
-            }
         }
     }
 }
