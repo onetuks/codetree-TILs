76bb96c7625583b94a0e1d56ce2a44df634e8711
Se0 <54990890+onetuks@users.noreply.github.com>
2024-12-27 23:01:50 +0900

    Title: 1차원 폭발 게임, Time: 104ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 241227/1차원 폭발 게임/The-1D-bomb-game.java
@@ -0,0 +1,75 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+
+    private static int n, m;
+    private static List<Integer> bombs;
+    
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        n = Integer.parseInt(sc.next());
+        m = Integer.parseInt(sc.next());
+
+        bombs = new ArrayList<>();
+        for (int i = 0; i < n; i ++) {
+            bombs.add(Integer.parseInt(sc.next()));
+        }
+
+        while (explosible()) {
+            explode();
+        }
+
+        if (bombs.isEmpty()) System.out.println(0);
+        else {
+            for (int bomb: bombs)
+                System.out.println(bomb);
+        }
+    }
+
+    private static boolean explosible() {
+        if (bombs.isEmpty()) return false;
+
+        int cnt = 0, val = 0;
+        for (int bomb: bombs) {
+            if (val == bomb) {
+                cnt++;
+                if (cnt >= m) return true;
+                continue;
+            }
+            cnt = 1;
+            val = bomb;
+        }
+
+        return false;
+    }
+
+    private static void explode() {
+        List<Integer> temp = new ArrayList<>();
+        
+        int cnt = 0, val = 0;
+        for (int i = 0; i < bombs.size(); i ++) {
+            if (bombs.get(i) == val) {
+                cnt++;
+                continue;
+            }
+
+            if (cnt < m) {
+                for (int j = 0; j < cnt; j ++) {
+                    temp.add(val);
+                }
+            }
+            cnt = 1;
+            val = bombs.get(i);
+        }
+
+        if (cnt < m) {
+            for (int j = 0; j < cnt; j ++) {
+                temp.add(val);
+            }
+        }
+
+        bombs = temp;
+    }
+}
