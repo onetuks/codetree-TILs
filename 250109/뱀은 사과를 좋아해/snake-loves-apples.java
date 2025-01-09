7d699bfb9bf96802a043ec192ab39becde147cea
Se0 <54990890+onetuks@users.noreply.github.com>
2025-01-09 15:09:27 +0900

    Title: 뱀은 사과를 좋아해, Time: 134ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 250109/뱀은 사과를 좋아해/snake-loves-apples.java
@@ -0,0 +1,92 @@
+import java.util.*;
+import java.io.*;
+
+class Pair {
+    int x;
+    int y;
+
+    Pair(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    boolean isOutOfRange(int n) {
+        return this.x < 0 || this.x >= n || this.y < 0 || this.y >= n;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Pair other = (Pair) o;
+        return this.x == other.x && this.y == other.y;
+    }
+
+    @Override
+    public String toString() {
+        return "x:" + this.x + " y:" + this.y;
+    }
+}
+
+public class Main {
+    private static final int[][] dlist = {{0, 1}, {0, -1}, {1, 0}, {-1, 0}};
+    private static final Map<Character, Integer> map = new HashMap<Character, Integer>() {{
+        put('R', 0);
+        put('L', 1);
+        put('D', 2);
+        put('U', 3);
+    }};
+
+    private static int n, m, k;
+    private static int[][] matrix;
+    private static Deque<Pair> deque;
+    
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        n = sc.nextInt();
+        m = sc.nextInt();
+        k = sc.nextInt();
+
+        matrix = new int[n][n];
+
+        for (int i = 0; i < m; i ++) {
+            int x = sc.nextInt() - 1;
+            int y = sc.nextInt() - 1;
+            matrix[x][y] = 1;
+        }
+
+        deque = new ArrayDeque<>();
+        deque.addFirst(new Pair(0, 0));
+
+        int answer = 0;
+
+        for (int l = 0; l < k; l ++) {
+            int d = map.get(sc.next().charAt(0));
+            int p = sc.nextInt();
+            boolean flag = false;
+
+            for (int i = 0; i < p; i ++) {
+                answer++;
+
+                Pair head = deque.peekFirst();
+                Pair nextHead = new Pair(head.x + dlist[d][0], head.y + dlist[d][1]);
+
+                if (nextHead.isOutOfRange(n)) {
+                    flag = true;
+                    break;
+                }
+                if (matrix[nextHead.x][nextHead.y] != 1) deque.peekLast();
+                if (deque.contains(nextHead)) {
+                    flag = true;
+                    break;
+                }
+                deque.addFirst(nextHead);
+            }
+
+            if (flag) break;
+        }
+
+        System.out.println(answer);
+    }
+}
