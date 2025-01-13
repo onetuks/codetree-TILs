5286a89c1aab509dfb5fcf22ab0f24a453978446
Se0 <54990890+onetuks@users.noreply.github.com>
2025-01-13 16:09:16 +0900

    Title: 벽이 있는 충돌 실험, Time: 293ms, Memory: 21MB, Status: Wrong Answer - CodeTree

--- 250113/벽이 있는 충돌 실험/collision-experiment-with-wall.java
+++ 250113/벽이 있는 충돌 실험/collision-experiment-with-wall.java
@@ -1,40 +1,19 @@
 import java.util.*;
 import java.io.*;
 
-class Pair {
-    int x;
-    int y;
-    int d;
-    
-    Pair(int x, int y, int d) {
-        this.x = x;
-        this.y = y;
-        this.d = d;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (o == this) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        Pair p = (Pair) o;
-        return this.x == p.x && this.y == p.y;
-    }
-}
-
 public class Main {
 
-    private static final int[][] dlist = {{-1, 0}, {0, -1}, {1, 0}, {0, 1}};
+    private static final int[][] dlist = {{}, {-1, 0}, {0, -1}, {1, 0}, {0, 1}};
     private static final Map<Character, Integer> map = new HashMap<Character, Integer>() {{
-        put('U', 0);
-        put('L', 1);
-        put('D', 2);
-        put('R', 3);
+        put('U', 1);
+        put('L', 2);
+        put('D', 3);
+        put('R', 4);
     }};
 
     private static int t, n, m;
     private static int[][] matrix;
-    private static List<Pair> pairs;
-
+    
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
@@ -42,18 +21,19 @@ public class Main {
 
         t = Integer.parseInt(st.nextToken());
 
-        for (int tc = 0; tc < t; tc ++) {
+        while (t-- > 0) {
             st = new StringTokenizer(br.readLine());
             n = Integer.parseInt(st.nextToken());
             m = Integer.parseInt(st.nextToken());
 
-            pairs = new ArrayList<>();
-            for (int i = 0; i < m; i ++) {
+            matrix = new int[n][n];
+
+            while (m-- > 0) {
                 st = new StringTokenizer(br.readLine());
-                int x = Integer.parseInt(st.nextToken()) - 1;
-                int y = Integer.parseInt(st.nextToken()) - 1;
+                int i = Integer.parseInt(st.nextToken()) - 1;
+                int j = Integer.parseInt(st.nextToken()) - 1;
                 int d = map.get(st.nextToken().charAt(0));
-                pairs.add(new Pair(x, y, d));
+                matrix[i][j] = d;
             }
 
             for (int i = 0; i <= n * 2; i ++) {
@@ -71,27 +51,31 @@ public class Main {
     }
 
     private static void moveAll() {
-        matrix = new int[n][n];
-        List<Pair> temp = new ArrayList<>();
+        int[][] temp = new int[n][n];
 
-        for (Pair pair: pairs) {
-            int d = pair.d;
-            int di = pair.x + dlist[d][0];
-            int dj = pair.y + dlist[d][1];
-            if (di < 0 || di >= n || dj < 0 || dj >= n) {
-                d = (d >= 2) ? (d - 2) : (d + 2);
-                di = pair.x;
-                dj = pair.y;
+        for (int i = 0; i < n; i ++) {
+            for (int j = 0; j < n; j ++) {
+                if (matrix[i][j] > 0) {
+                    int d = matrix[i][j];
+                    int di = i + dlist[d][0];
+                    int dj = j + dlist[d][1];
+                    if (di < 0 || di >= n || dj < 0 || dj >= n) {
+                        d = d > 2 ? (d - 2) : (d + 2);
+                        di = i;
+                        dj = j;
+                    }
+                    if (temp[di][dj] > 0) {
+                        temp[di][dj] = 0;
+                    } else {
+                        temp[di][dj] = d;
+                    }
+                }
             }
-            Pair nPair = new Pair(di, dj, d);
-            matrix[di][dj]++;
-            temp.add(nPair);
         }
 
-        pairs.clear();
-        for (Pair pair: temp) {
-            if (matrix[pair.x][pair.y] == 1) {
-                pairs.add(pair);
+        for (int i = 0; i < n; i ++) {
+            for (int j = 0; j < n; j ++) {
+                matrix[i][j] = temp[i][j];
             }
         }
     }
