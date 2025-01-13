5612271a89e04e80fa5499ab9556242b8d8d1601
Se0 <54990890+onetuks@users.noreply.github.com>
2025-01-13 15:18:25 +0900

    Title: 벽이 있는 충돌 실험, Time: 3100ms, Memory: 184MB, Status: Time Limit Exceed - CodeTree

--- 250113/벽이 있는 충돌 실험/collision-experiment-with-wall.java
+++ 250113/벽이 있는 충돌 실험/collision-experiment-with-wall.java
@@ -3,17 +3,17 @@ import java.io.*;
 
 public class Main {
 
-    private static final int[][] dlist = {{}, {-1, 0}, {0, -1}, {1, 0}, {0, 1}};
+    private static final int[][] dlist = {{-1, 0}, {0, -1}, {1, 0}, {0, 1}};
     private static final Map<Character, Integer> map = new HashMap<Character, Integer>() {{
-        put('U', 1);
-        put('L', 2);
-        put('D', 3);
-        put('R', 4);
+        put('U', 0);
+        put('L', 1);
+        put('D', 2);
+        put('R', 3);
     }};
 
     private static int t, n, m;
-    private static int[][] currGrid;
-    private static int[][] nextGrid;
+    private static int[][] matrix;
+    private static int[][] direct;
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
@@ -24,12 +24,15 @@ public class Main {
             n = sc.nextInt();
             m = sc.nextInt();
 
-            currGrid = new int[n][n];
+            matrix = new int[n][n];
+            direct = new int[n][n];
+
             for (int i = 0; i < m; i ++) {
                 int x = sc.nextInt() - 1;
                 int y = sc.nextInt() - 1;
                 int d = map.get(sc.next().charAt(0));
-                currGrid[x][y] = d;
+                matrix[x][y] = 1;
+                direct[x][y] = d;
             }
 
             for (int i = 0; i <= n * n; i ++) {
@@ -39,7 +42,7 @@ public class Main {
             int answer = 0;
             for (int i = 0; i < n; i ++) {
                 for (int j = 0; j < n; j ++) {
-                    if (currGrid[i][j] > 0) answer++;
+                    if (matrix[i][j] > 0) answer++;
                 }
             }
             System.out.println(answer);
@@ -47,38 +50,52 @@ public class Main {
     }
 
     private static void moveAll() {
-        nextGrid = new int[n][n];
-
+        int[][] temp = new int[n][n];
+        int[][] demp = new int[n][n];
+        
         for (int i = 0; i < n; i ++) {
             for (int j = 0; j < n; j ++) {
-                if (currGrid[i][j] > 0) {
-                    int d = currGrid[i][j];
+                if (matrix[i][j] > 0) {
+                    int d = direct[i][j];
                     int di = i + dlist[d][0];
                     int dj = j + dlist[d][1];
+                    // System.out.printf("1) i:%d j:%d di:%d dj:%d d:%d\n", i, j, di, dj, d);
                     if (di < 0 || di >= n || dj < 0 || dj >= n) {
-                        d = d > 2 ? (d - 2) : (d + 2);
-                        if (nextGrid[i][j] > 0) {
-                            nextGrid[i][j] = 0;
-                            continue;
-                        }
-                        nextGrid[i][j] = d;
-                        continue;
+                        d = d >= 2 ? (d - 2) : (d + 2);
+                        di = i;
+                        dj = j;
                     }
-                    if (nextGrid[di][dj] > 0) {
-                        nextGrid[di][dj] = 0;
-                        continue;
-                    }
-                    nextGrid[di][dj] = d;
+                    // System.out.printf("2) i:%d j:%d di:%d dj:%d d:%d\n", i, j, di, dj, d);
+                    temp[di][dj]++;
+                    demp[di][dj] = d;
                 }
             }
         }
 
+        // for (int i = 0; i < n; i ++) {
+        //     for (int j = 0; j < n; j ++) {
+        //         System.out.print(temp[i][j] + " ");
+        //     }
+        //     System.out.println();
+        // }
+
         for (int i = 0; i < n; i ++) {
             for (int j = 0; j < n; j ++) {
-                currGrid[i][j] = nextGrid[i][j];
-                // System.out.printf("%d ", nextGrid[i][j]);
+                if (temp[i][j] == 1) {
+                    matrix[i][j] = temp[i][j];
+                    direct[i][j] = demp[i][j];
+                } else {
+                    matrix[i][j] = 0;
+                    direct[i][j] = 0;
+                }
             }
-            // System.out.println();
         }
+        
+        // for (int i = 0; i < n; i ++) {
+        //     for (int j = 0; j < n; j ++) {
+        //         System.out.print(matrix[i][j] + " ");
+        //     }
+        //     System.out.println();
+        // }
     }
 }
