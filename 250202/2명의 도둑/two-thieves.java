513f68bdbe3ca59f10525801f9d2ea168a973460
Se0 <54990890+onetuks@users.noreply.github.com>
2025-02-02 16:27:02 +0900

    Title: 2명의 도둑, Time: 117ms, Memory: 10MB, Status: Passed - CodeTree

--- 250202/2명의 도둑/two-thieves.java
+++ 250202/2명의 도둑/two-thieves.java
@@ -5,6 +5,11 @@ public class Main {
 
     private static int n, m, c;
     private static int[][] matrix;
+    private static int[][] bestVal;
+
+    private static int maxVal;
+    private static int[] temp;
+
     private static int answer = Integer.MIN_VALUE;
 
     public static void main(String[] args) {
@@ -16,71 +21,62 @@ public class Main {
 
         matrix = new int[n][n];
         for (int i =0;i < n; i++)
-            for (int j =0; j < n; j++)
+            for (int j = 0; j < n; j++)
                 matrix[i][j] = sc.nextInt();
+
+        preprocessing();
+
+        // // debug
+        // for (int i = 0; i < n; i ++)
+        //     System.out.println(Arrays.toString(bestVal[i]));
+        // // debug
 
         solve();
 
         System.out.println(answer);
     }
 
+    private static void preprocessing() {
+        bestVal = new int[n][n];
+
+        for (int i = 0; i < n; i ++) {
+            for (int j = 0; j <= n - m; j ++) {
+                bestVal[i][j] = findMaxVal(i, j);
+            }
+        }
+    }
+
+    private static int findMaxVal(int x, int y) {
+        temp = new int[m];
+        for (int i = y; i < y + m; i ++)
+            temp[i - y] = matrix[x][i];
+
+        maxVal = 0;
+        findMaxSum(0, 0, 0);
+        return maxVal;
+    }
+
+    private static void findMaxSum(int idx, int weight, int val) {
+        if (idx == m) {
+            if (weight <= c)
+                maxVal = Math.max(maxVal, val);
+            return;
+        }
+
+        findMaxSum(idx + 1, weight, val);
+        findMaxSum(idx + 1, weight + temp[idx], val + temp[idx] * temp[idx]);
+    }
+
     private static void solve() {
         for (int i = 0; i < n; i ++) {
             for (int j = 0; j <= n - m; j ++) {
-                List<Integer> list1 = gather(i, j);
                 for (int x = 0; x < n; x ++) {
                     for (int y = 0; y <= n - m; y ++) {
                         if (i == x && ((j <= y && y < j + m) || (y <= j && j < y + m))) continue;
-                        List<Integer> list2 = gather(x, y);
-
-                        // debug
-                        // if (answer < getScore(list1, list2)) {
-                        //     System.out.println(i + " " + j + " " + x + " " + y + ": " + getScore(list1, list2));
-                        //     for (int l: list1) System.out.print(l + " " );
-                        //     System.out.println();
-                        //     for (int l: list2) System.out.print(l + " " );
-                        //     System.out.println();
-                        // }
-                        // debug
-
-                        answer = Math.max(answer, getScore(list1, list2));
+                        answer = Math.max(answer, bestVal[i][j] + bestVal[x][y]);
                     }
                 }
             }
         }
-    }
-
-    private static List<Integer> gather(int x, int y) {
-        int maxSum = Integer.MIN_VALUE;
-        List<Integer> maxList = new ArrayList<>();
-
-        List<Integer> vals = new ArrayList<>();
-        for (int i = y; i < y + m; i ++)
-            vals.add(matrix[x][i]);
-
-        for (int bitmask = 0; bitmask < (1 << vals.size()); bitmask ++) {
-            int sum = 0;
-            List<Integer> temp = new ArrayList<>();
-            for (int i = 0; i < vals.size(); i ++) {
-                if ((bitmask & (1 << i)) == 0) continue;
-                int val = vals.get(i);
-                sum += val;
-                temp.add(val);
-            }
-
-            if (sum <= c && sum > maxSum) {
-                maxSum = sum;
-                maxList = temp;
-            }
-        }
-
-        return maxList;
-    }
-
-    private static int getScore(List<Integer> list1, List<Integer> list2) {
-        int result = 0;
-        for (int l: list1) result += l * l;
-        for (int l: list2) result += l * l;
-        return result;
     }
 }
