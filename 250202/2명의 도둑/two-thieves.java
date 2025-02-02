b3a1c034b6bfda08bbeb68c338e98ebb6977a4ef
Se0 <54990890+onetuks@users.noreply.github.com>
2025-02-02 16:39:45 +0900

    Title: 2명의 도둑, Time: 122ms, Memory: 10MB, Status: Passed - CodeTree

--- 250202/2명의 도둑/two-thieves.java
+++ 250202/2명의 도둑/two-thieves.java
@@ -3,12 +3,13 @@ import java.io.*;
 
 public class Main {
 
+    private static final int INIT = -1;
+
     private static int n, m, c;
     private static int[][] matrix;
     private static int[][] bestVal;
 
     private static int maxVal;
-    private static int[] temp;
 
     private static int answer = Integer.MIN_VALUE;
 
@@ -20,51 +21,39 @@ public class Main {
         c = sc.nextInt();
 
         matrix = new int[n][n];
-        for (int i =0;i < n; i++)
-            for (int j = 0; j < n; j++)
+        bestVal= new int[n][n];
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
                 matrix[i][j] = sc.nextInt();
-
-        preprocessing();
-
-        // // debug
-        // for (int i = 0; i < n; i ++)
-        //     System.out.println(Arrays.toString(bestVal[i]));
-        // // debug
+                bestVal[i][j] = INIT;
+            }
+        }
 
         solve();
 
         System.out.println(answer);
     }
 
-    private static void preprocessing() {
-        bestVal = new int[n][n];
-
-        for (int i = 0; i < n; i ++) {
-            for (int j = 0; j <= n - m; j ++) {
-                bestVal[i][j] = findMaxVal(i, j);
-            }
-        }
-    }
-
     private static int findMaxVal(int x, int y) {
-        temp = new int[m];
-        for (int i = y; i < y + m; i ++)
-            temp[i - y] = matrix[x][i];
+        if (bestVal[x][y] != INIT) {
+            return bestVal[x][y];
+        }
 
         maxVal = 0;
-        findMaxSum(0, 0, 0);
-        return maxVal;
+        findMaxSum(x, y, 0, 0, 0);
+        bestVal[x][y] = maxVal;
+        return bestVal[x][y];
     }
 
-    private static void findMaxSum(int idx, int weight, int val) {
+    private static void findMaxSum(int x, int y, int idx, int weight, int val) {
         if (idx == m) {
             if (weight <= c)
                 maxVal = Math.max(maxVal, val);
             return;
         }
 
-        findMaxSum(idx + 1, weight, val);
-        findMaxSum(idx + 1, weight + temp[idx], val + temp[idx] * temp[idx]);
+        findMaxSum(x, y, idx + 1, weight, val);
+        findMaxSum(x, y, idx + 1, weight + matrix[x][y + idx], val + matrix[x][y + idx] * matrix[x][y + idx]);
     }
 
     private static void solve() {
@@ -73,7 +62,7 @@ public class Main {
                 for (int x = 0; x < n; x ++) {
                     for (int y = 0; y <= n - m; y ++) {
                         if (i == x && ((j <= y && y < j + m) || (y <= j && j < y + m))) continue;
-                        answer = Math.max(answer, bestVal[i][j] + bestVal[x][y]);
+                        answer = Math.max(answer, findMaxVal(i, j) + findMaxVal(x, y));
                     }
                 }
             }
