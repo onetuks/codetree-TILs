99711bee48b1d5a99b5ed505f17db1cad955af8c
Se0 <54990890+onetuks@users.noreply.github.com>
2025-02-10 14:13:48 +0900

    Title: 최소 점프 횟수, Time: 124ms, Memory: 10MB, Status: Runtime Error - CodeTree

---
+++ 250210/최소 점프 횟수/min-num-of-jumps.java
@@ -0,0 +1,33 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    
+    private static int n;
+    private static int[] jumps;
+    private static int answer = Integer.MAX_VALUE;
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        n = sc.nextInt();
+
+        jumps = new int[n];
+        for (int i = 0; i < n; i ++)
+            jumps[i] = sc.nextInt();
+
+        backtrack(0, 0);
+
+        System.out.println(answer == Integer.MAX_VALUE ? -1 : answer);
+    }
+
+    private static void backtrack(int idx, int cnt) {
+        if (idx == n - 1) {
+            answer = Math.min(answer, cnt);
+            return;
+        }
+
+        for (int step = 1; step <= jumps[idx]; step ++)
+            backtrack(idx + step, cnt + 1);
+    }
+}
