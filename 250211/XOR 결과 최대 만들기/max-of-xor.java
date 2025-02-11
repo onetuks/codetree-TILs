8b26cc6e5dd1ed1a3ba3c967de69ce1dcb4e2512
Se0 <54990890+onetuks@users.noreply.github.com>
2025-02-11 13:16:05 +0900

    Title: XOR 결과 최대 만들기, Time: 3052ms, Memory: 10MB, Status: Time Limit Exceed - CodeTree

--- 250211/XOR 결과 최대 만들기/max-of-xor.java
+++ 250211/XOR 결과 최대 만들기/max-of-xor.java
@@ -5,7 +5,7 @@ public class Main {
 
     private static int n, m;
     private static int[] numbers;
-    private static List<Integer> chosen = new ArrayList<>();
+
     private static int answer = 0;
 
     public static void main(String[] args) {
@@ -18,28 +18,20 @@ public class Main {
         for (int i = 0; i < n; i ++) 
             numbers[i] = sc.nextInt();
 
-        choose(0);
+        choose(0, 0, 0);
 
         System.out.println(answer);
     }
 
-    private static void choose(int idx) {
-        if (chosen.size() == m) {
-            answer = Math.max(answer, getXorResult());
+    private static void choose(int idx, int cnt, int val) {
+        if (cnt == m) {
+            answer = Math.max(answer, val);
             return;
         }
 
         for (int i = idx; i < n; i ++) {
-            chosen.add(numbers[i]);
-            choose(i + 1);
-            chosen.remove(chosen.size() - 1);
+            choose(i + 1, cnt + 1, val ^ numbers[i]);
+            choose(i + 1, cnt, val);
         }
-    }
-
-    private static int getXorResult() {
-        int result = 0;
-        for (int c: chosen)
-            result ^= c;
-        return result;
     }
 }
