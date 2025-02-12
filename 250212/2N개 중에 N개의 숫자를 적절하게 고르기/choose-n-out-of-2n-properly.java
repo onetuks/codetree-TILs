7a0ffa721422a72d2479ac1f4123b40406fcda64
Se0 <54990890+onetuks@users.noreply.github.com>
2025-02-12 17:08:59 +0900

    Title: 2N개 중에 N개의 숫자를 적절하게 고르기, Time: 136ms, Memory: 10MB, Status: Passed - CodeTree

--- 250212/2N개 중에 N개의 숫자를 적절하게 고르기/choose-n-out-of-2n-properly.java
+++ 250212/2N개 중에 N개의 숫자를 적절하게 고르기/choose-n-out-of-2n-properly.java
@@ -4,28 +4,43 @@ import java.io.*;
 public class Main {
     
     private static int n;
-    private static int[] numbers;
+    private static List<Integer> numbers = new ArrayList<>();
+    private static Set<Integer> group = new HashSet<>();
+    private static int totalSum = 0;
+    private static int answer = Integer.MAX_VALUE;
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
         n = sc.nextInt();
 
-        numbers = new int[n * 2];
-        for (int i = 0; i < 2 * n; i ++)
-            numbers[i] = sc.nextInt();
-
-        Arrays.sort(numbers);
-
-        int sum1 = 0, sum2 = 0;
-
-        for (int i = 2 * n - 1; i >= 0; i --) {
-            if (sum1 < sum2)
-                sum1 += numbers[i];
-            else
-                sum2 += numbers[i];
+        for (int i = 0; i < 2 * n; i++) {
+            int num = sc.nextInt();
+            numbers.add(num);
+            totalSum += num;
         }
 
-        System.out.println(Math.abs(sum1 - sum2));
+        makeGroup(0);
+
+        System.out.print(answer);
+    }
+
+    private static void makeGroup(int idx) {
+        if (group.size() == n) {
+            answer = Math.min(answer, getDiff());
+            return;
+        }
+
+        for (int i = idx; i < 2 * n; i++) {
+            group.add(numbers.get(i));
+            makeGroup(i + 1);
+            group.remove(numbers.get(i));
+        }
+    }
+
+    private static int getDiff() {
+        int sumOfGroup = 0;
+        for (int g: group) sumOfGroup += g;
+        return Math.abs(sumOfGroup - (totalSum - sumOfGroup));
     }
 }
