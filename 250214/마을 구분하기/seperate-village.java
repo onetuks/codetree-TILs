bf2b29ea88db1aceefaa48a60e85bd290e9d90bd
Se0 <54990890+onetuks@users.noreply.github.com>
2025-02-14 09:50:01 +0900

    Title: 마을 구분하기, Time: 261ms, Memory: 14MB, Status: Passed - CodeTree

--- 250214/마을 구분하기/seperate-village.java
+++ 250214/마을 구분하기/seperate-village.java
@@ -8,8 +8,7 @@ public class Main {
     private static int n;
     private static int[][] matrix;
     private static boolean[][] visited;
-
-    private static int villageCnt = 0;
+    private static int villageCnt;
     private static List<Integer> populations = new ArrayList<>();
 
     public static void main(String[] args) throws IOException {
@@ -28,30 +27,29 @@ public class Main {
         for (int i = 0; i < n; i ++) {
             for (int j = 0; j < n; j ++) {
                 if (!visited[i][j] && matrix[i][j] == 1) {
-                    villageCnt++;
                     visited[i][j] = true;
-                    int cnt = dfs(i, j);
-                    populations.add(cnt);
+                    villageCnt = 1;
+                    dfs(i, j);
+                    populations.add(villageCnt);
                 }
             }
         }
 
         populations.sort((a, b) -> a - b);
-        System.out.println(villageCnt);
+        System.out.println(populations.size());
         for (int population: populations)
             System.out.println(population);
     }
 
-    private static int dfs(int i, int j) {
-        int cnt = 1;
+    private static void dfs(int i, int j) {
         for (int[] d: DLIST) {
             int di = i + d[0];
             int dj = j + d[1];
             if (di < 0 || di >= n || dj < 0 || dj >= n) continue;
             if (visited[di][dj] || matrix[di][dj] == 0) continue;
             visited[di][dj] = true;
-            cnt += dfs(di, dj);
+            villageCnt++;
+            dfs(di, dj);
         }
-        return cnt;
     }
 }
