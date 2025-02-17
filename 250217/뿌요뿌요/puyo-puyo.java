79c066eea0ad7696b0cac55c4ecc6aa1a71042dd
Se0 <54990890+onetuks@users.noreply.github.com>
2025-02-17 11:43:52 +0900

    Title: 뿌요뿌요, Time: 90ms, Memory: 9MB, Status: Wrong Answer - CodeTree

---
+++ 250217/뿌요뿌요/puyo-puyo.java
@@ -0,0 +1,59 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+
+    private static final int[][] DLIST = {{-1, 0}, {0, -1}, {1, 0}, {0, 1}};
+
+    private static int n;
+    private static int[][] matrix;
+    private static boolean[][] visited;
+
+    private static int explodedBombCount = 0;
+    private static int maxBlockSize = 0;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        n = Integer.parseInt(br.readLine());
+
+        matrix = new int[n][n];
+        visited = new boolean[n][n];
+        for (int i = 0; i < n; i ++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < n; j ++) {
+                matrix[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+
+        explode();
+
+        System.out.println(explodedBombCount + " " + maxBlockSize);
+    }
+
+    private static void explode() {
+        for (int i = 0; i< n; i ++) {
+            for (int j = 0; j < n;j ++) {
+                if (visited[i][j]) continue;
+                int count = dfs(i, j) - 1;
+                maxBlockSize = Math.max(maxBlockSize, count);
+                if (count >= 4)
+                    explodedBombCount++;
+            }
+        }
+    }
+
+    private static int dfs(int i, int j) {
+        int cnt = 1;
+        int val = matrix[i][j];
+        for (int[] d: DLIST) {
+            int di = i + d[0];
+            int dj = j + d[1];
+            if (di < 0 || di >= n || dj < 0 || dj >= n) continue;
+            if (visited[di][dj] || matrix[di][dj] != val) continue;
+            visited[di][dj] = true;
+            cnt += dfs(di, dj);
+        }
+        return cnt;
+    }
+}
