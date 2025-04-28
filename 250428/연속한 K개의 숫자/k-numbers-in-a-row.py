20679f45ef57eb2bffce27b9d746ab58f1310931
Se0 <54990890+onetuks@users.noreply.github.com>
2025-04-28 23:22:15 +0900

    Title: 연속한 K개의 숫자, Time: 50ms, Memory: 16MB, Status: Wrong Answer - Codetree

--- 250428/연속한 K개의 숫자/k-numbers-in-a-row.py
+++ 250428/연속한 K개의 숫자/k-numbers-in-a-row.py
@@ -1,16 +1,16 @@
 n, k, b = map(int, input().split())
-missing = sorted(list(map(int, input().split())))
+missing = [int(input()) for _ in range(b)]
 
 psum = [0]
 s = 0
 for i in range(1, n + 1):
-    if i in missing: continue
-    s += i
+    if i not in missing:
+        s += i
     psum.append(s)
 
 min_diff = float('inf')
 min_idx = 0
-for i in range(n - k):
+for i in range(n - k + 1):
     j = i + k
     real = psum[j] - psum[i]
     expt = (j + i + 1) * k / 2
@@ -20,7 +20,7 @@ for i in range(n - k):
 
 ans = 0
 for m in missing:
-    if min_idx <= m <= min_idx + k:
+    if min_idx <= m < min_idx + k:
         ans += 1
 
 print(ans)
