de363a3934d331126e07ee1bde696b42a5098ee2
Se0 <54990890+onetuks@users.noreply.github.com>
2025-04-29 00:16:29 +0900

    Title: 범위 내에 있는 점의 수 2, Time: 296ms, Memory: 36MB, Status: Passed - Codetree

--- 250429/범위 내에 있는 점의 수 2/the-number-of-points-within-the-range-2.py
+++ 250429/범위 내에 있는 점의 수 2/the-number-of-points-within-the-range-2.py
@@ -1,13 +1,13 @@
 n, q = map(int, input().split())
-bound = int(1e6) + 1
+bound = int(1e6) + 2
 plist = [0] * bound
 psum = [0] * bound
 
 for p in list(map(int, input().split())):
-    plist[p] = 1
+    plist[p + 1] = 1
 for i in range(1, bound):
     psum[i] = psum[i - 1] + plist[i]
 
 for _ in range(q):
     a, b = map(int, input().split())
-    print(psum[b] - psum[a - 1])
+    print(psum[b + 1] - psum[a])
