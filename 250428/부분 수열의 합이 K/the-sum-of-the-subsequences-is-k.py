bc3dfc3d8fdc7167a84f65383e05543c0fe8f7c8
Se0 <54990890+onetuks@users.noreply.github.com>
2025-04-28 22:47:21 +0900

    Title: 부분 수열의 합이 K, Time: 53ms, Memory: 17MB, Status: Passed - Codetree

--- 250428/부분 수열의 합이 K/the-sum-of-the-subsequences-is-k.py
+++ 250428/부분 수열의 합이 K/the-sum-of-the-subsequences-is-k.py
@@ -7,9 +7,9 @@ arr = list(map(int, input().split()))
 
 ans = 0
 
-pfsum = [0, arr[0]]
+pfsum = [0] * (n + 1)
 for i in range(1, n):
-    pfsum.append(pfsum[-1] + arr[i])
+    pfsum[i] = (pfsum[i - 1] + arr[i])
 
 for i in range(n + 1):
     for j in range(n + 1):
