ae5afdf166c02a11289c38a291a5e993982464b2
Se0 <54990890+onetuks@users.noreply.github.com>
2025-04-30 22:24:15 +0900

    Title: 점 개수 세기 3, Time: 1011ms, Memory: 46MB, Status: Time Limit Exceed - Codetree

---
+++ 250430/점 개수 세기 3/count-number-of-points-3.py
@@ -0,0 +1,20 @@
+from sys import stdin
+from sortedcontainers import SortedSet
+from bisect import bisect_left
+
+input = stdin.readline
+
+n, q = map(int, input().split())
+points = SortedSet(list(map(int, input().split())))
+queries = [tuple(map(int, input().split())) for _ in range(q)]
+
+mapper = dict()
+idx = 0
+for point in points:
+    mapper[point] = idx
+    idx += 1
+
+for a, b in queries:
+    l = mapper[points[bisect_left(points, a)]]
+    r = mapper[points[bisect_left(points, b)]]
+    print(r - l + 1)
