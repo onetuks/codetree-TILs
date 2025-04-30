b2d9535d0b3eeabe87aeb996f1ed9f546b430068
Se0 <54990890+onetuks@users.noreply.github.com>
2025-04-30 17:22:04 +0900

    Title: 구간에 속한 문자의 개수, Time: 143ms, Memory: 39MB, Status: Runtime Error - Codetree

--- 250430/구간에 속한 문자의 개수/the-number-of-characters-in-the-interval.py
+++ 250430/구간에 속한 문자의 개수/the-number-of-characters-in-the-interval.py
@@ -3,30 +3,28 @@ grid = [" " * (n + 1)]
 for _ in range(n):
     grid.append(" " + input())
 
-def get_cnt(i, j, key):
-    if key == "a":
-        return psum[i-1][j][0] + psum[i][j-1][0] - psum[i-1][j-1][0]
-    elif key == "b":
-        return psum[i-1][j][1] + psum[i][j-1][1] - psum[i-1][j-1][1]
-    else:
-        return psum[i-1][j][2] + psum[i][j-1][2] - psum[i-1][j-1][2]
-
 # Please write your code here.
-psum = [[[0, 0, 0] for _ in range(n + 1)] for _ in range(n + 1)]
+apsum = [[0 for _ in range(n + 1)] for _ in range(n + 1)]
+bpsum = [[0 for _ in range(n + 1)] for _ in range(n + 1)]
+cpsum = [[0 for _ in range(n + 1)] for _ in range(n + 1)]
 for i in range(1, n+1):
     for j in range(1, n + 1):
-        a_cnt, b_cnt, c_cnt = get_cnt(i, j, "a"), get_cnt(i, j, "b"), get_cnt(i, j, "c")
+        a_cnt = apsum[i-1][j] + apsum[i][j-1] - apsum[i-1][j-1]
+        b_cnt = bpsum[i-1][j] + bpsum[i][j-1] - bpsum[i-1][j-1]
+        c_cnt = cpsum[i-1][j] + cpsum[i][j-1] - cpsum[i-1][j-1]
         if grid[i][j] == "a":
             a_cnt += 1
         elif grid[i][j] == "b":
             b_cnt += 1
         else:
             c_cnt += 1
-        psum[i][j] = [a_cnt, b_cnt, c_cnt]
+        apsum[i][j] = a_cnt
+        bpsum[i][j] = b_cnt
+        cpsum[i][j] = c_cnt
 
 for _ in range(k):
     i, j, x, y = map(int, input().split())
-    a_cnt = psum[x][y][0] - psum[x][j-1][0] - psum[i-1][y][0] + psum[i-1][j-1][0]
-    b_cnt = psum[x][y][1] - psum[x][j-1][1] - psum[i-1][y][1] + psum[i-1][j-1][1]
-    c_cnt = psum[x][y][2] - psum[x][j-1][2] - psum[i-1][y][2] + psum[i-1][j-1][2]
+    a_cnt = apsum[x][y] - apsum[x][j-1] - apsum[i-1][y] + apsum[i-1][j-1]
+    b_cnt = bpsum[x][y] - bpsum[x][j-1] - bpsum[i-1][y] + bpsum[i-1][j-1]
+    c_cnt = cpsum[x][y] - cpsum[x][j-1] - cpsum[i-1][y] + cpsum[i-1][j-1]
     print(a_cnt, b_cnt, c_cnt)
