c76ea154aef1264cdcc25a0def9aebcb74f35345
Se0 <54990890+onetuks@users.noreply.github.com>
2025-04-30 16:06:23 +0900

    Title: 최대 직사각형 합, Time: 388ms, Memory: 21MB, Status: Passed - Codetree

--- 250430/최대 직사각형 합/max-rect-sum-in-grid.py
+++ 250430/최대 직사각형 합/max-rect-sum-in-grid.py
@@ -10,14 +10,11 @@ def get_sum(x, y, i, j):
     return psum[i][j] - psum[i][y-1] - psum[x-1][j] + psum[x-1][y-1]
 
 def get_max_sum(x1, x2):
-    dp = [0] * (n + 1)
+    dp = [-1e9] * (n + 1)
     for y in range(1, n + 1):
         val = get_sum(x1, y, x2, y)
         dp[y] = max(val, dp[y-1] + val)
-    max_sum = -1e9
-    for y in range(1, n + 1):
-        max_sum = max(max_sum, dp[y])
-    return max_sum
+    return max(dp)
 
 psum = [[0 for _ in range(n + 1)] for _ in range(n + 1)]
 for i in range(1, n + 1):
