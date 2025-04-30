fb344562a2dc721c2a84b6b7afd204e327ac615c
Se0 <54990890+onetuks@users.noreply.github.com>
2025-04-30 17:17:57 +0900

    Title: 구간에 속한 문자의 개수, Time: 450ms, Memory: 80MB, Status: Memory Limit Exceed - Codetree

--- 250430/구간에 속한 문자의 개수/the-number-of-characters-in-the-interval.py
+++ 250430/구간에 속한 문자의 개수/the-number-of-characters-in-the-interval.py
@@ -1,15 +1,3 @@
-class Info:
-    def __init__(self, a_cnt, b_cnt, c_cnt):
-        self.a_cnt = a_cnt
-        self.b_cnt = b_cnt
-        self.c_cnt = c_cnt
-
-    def __str__(self):
-        return str(self.a_cnt) + " " + str(self.b_cnt) + " " + str(self.c_cnt)
-
-    def print(self):
-        print(self.a_cnt, self.b_cnt, self.c_cnt)
-
 n, m, k = map(int, input().split())
 grid = [" " * (n + 1)]
 for _ in range(n):
@@ -17,14 +5,14 @@ for _ in range(n):
 
 def get_cnt(i, j, key):
     if key == "a":
-        return psum[i-1][j].a_cnt + psum[i][j-1].a_cnt - psum[i-1][j-1].a_cnt
+        return psum[i-1][j][0] + psum[i][j-1][0] - psum[i-1][j-1][0]
     elif key == "b":
-        return psum[i-1][j].b_cnt + psum[i][j-1].b_cnt - psum[i-1][j-1].b_cnt
+        return psum[i-1][j][1] + psum[i][j-1][1] - psum[i-1][j-1][1]
     else:
-        return psum[i-1][j].c_cnt + psum[i][j-1].c_cnt - psum[i-1][j-1].c_cnt
+        return psum[i-1][j][2] + psum[i][j-1][2] - psum[i-1][j-1][2]
 
 # Please write your code here.
-psum = [[Info(0, 0, 0) for _ in range(n + 1)] for _ in range(n + 1)]
+psum = [[[0, 0, 0] for _ in range(n + 1)] for _ in range(n + 1)]
 for i in range(1, n+1):
     for j in range(1, n + 1):
         a_cnt, b_cnt, c_cnt = get_cnt(i, j, "a"), get_cnt(i, j, "b"), get_cnt(i, j, "c")
@@ -34,11 +22,11 @@ for i in range(1, n+1):
             b_cnt += 1
         else:
             c_cnt += 1
-        psum[i][j] = Info(a_cnt, b_cnt, c_cnt)
+        psum[i][j] = [a_cnt, b_cnt, c_cnt]
 
 for _ in range(k):
     i, j, x, y = map(int, input().split())
-    a_cnt = psum[x][y].a_cnt - psum[x][j-1].a_cnt - psum[i-1][y].a_cnt + psum[i-1][j-1].a_cnt
-    b_cnt = psum[x][y].b_cnt - psum[x][j-1].b_cnt - psum[i-1][y].b_cnt + psum[i-1][j-1].b_cnt
-    c_cnt = psum[x][y].c_cnt - psum[x][j-1].c_cnt - psum[i-1][y].c_cnt + psum[i-1][j-1].c_cnt
+    a_cnt = psum[x][y][0] - psum[x][j-1][0] - psum[i-1][y][0] + psum[i-1][j-1][0]
+    b_cnt = psum[x][y][1] - psum[x][j-1][1] - psum[i-1][y][1] + psum[i-1][j-1][1]
+    c_cnt = psum[x][y][2] - psum[x][j-1][2] - psum[i-1][y][2] + psum[i-1][j-1][2]
     print(a_cnt, b_cnt, c_cnt)
