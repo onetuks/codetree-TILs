f5a02c0d1acf7795988bdebfdce4b9c6a676a810
Se0 <54990890+onetuks@users.noreply.github.com>
2025-05-02 16:35:59 +0900

    Title: 점 개수 세기, Time: 1058ms, Memory: 45MB, Status: Time Limit Exceed - Codetree

---
+++ 250502/점 개수 세기/count-number-of-points.py
@@ -0,0 +1,22 @@
+from sortedcontainers import SortedSet
+
+n, q = map(int, input().split())
+psum = [0] * (n + 1)
+mapper = dict()
+points = SortedSet(list(map(int, input().split())))
+queries = [tuple(map(int, input().split())) for _ in range(q)]
+
+cnt = 1
+for p in points:
+    mapper[p] = cnt
+    psum[cnt] += 1
+    cnt += 1
+
+for i in range(1, n+1):
+    psum[i] += psum[i-1]
+
+for a, b in queries: 
+    na = points.bisect_left(a) + 1
+    nb = points.bisect_right(b)
+    ans = psum[nb] - psum[na-1]
+    print(ans)
