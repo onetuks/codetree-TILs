3f9332eb35f4817116d45b2ad81eb8d9e40b7826
Se0 <54990890+onetuks@users.noreply.github.com>
2025-05-02 19:47:56 +0900

    Title: 점 개수 세기, Time: 228ms, Memory: 34MB, Status: Passed - Codetree

--- 250502/점 개수 세기/count-number-of-points.py
+++ 250502/점 개수 세기/count-number-of-points.py
@@ -4,23 +4,12 @@ from bisect import bisect_left, bisect_right
 input = stdin.readline
 
 n, q = map(int, input().split())
-psum = [0] * (n + 1)
-mapper = dict()
 points = list(map(int, input().split()))
+queries = [tuple(map(int, input().split())) for _ in range(q)]
+
 points.sort()
 
-cnt = 1
-for p in points:
-    mapper[p] = cnt
-    psum[cnt] += 1
-    cnt += 1
-
-for i in range(1, n+1):
-    psum[i] += psum[i-1]
-
-for _ in range(q):
-    a, b = map(int, input().split())
-    na = bisect_left(points, a)
-    nb = bisect_right(points, b)
-    ans = psum[nb] - psum[na]
-    print(ans)
+for a, b in queries:
+    ia = bisect_left(points, a)
+    ib = bisect_right(points, b)
+    print(ib - ia)
