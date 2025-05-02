6580d04c24625f3ca61e1aa33cde11ed927b983b
Se0 <54990890+onetuks@users.noreply.github.com>
2025-05-02 16:26:48 +0900

    Title: 점 개수 세기 2, Time: 1564ms, Memory: 280MB, Status: Memory Limit Exceed - Codetree

---
+++ 250502/점 개수 세기 2/count-number-of-points-2.py
@@ -0,0 +1,40 @@
+from sortedcontainers import SortedSet
+from bisect import bisect_left, bisect_right
+
+n, q = map(int, input().split())
+nums = SortedSet()
+mapper = dict()
+psum = [[0 for _ in range(2*(n+1))] for _ in range(2*(n+1))]
+
+points = [tuple(map(int, input().split())) for _ in range(n)]
+queries = [tuple(map(int, input().split())) for _ in range(q)]
+
+def get_lower_bound(num):
+    return nums.bisect_left(num) + 1
+
+def get_upper_bound(num):
+    return nums.bisect_right(num)
+
+for x, y in points:
+    nums.add(x)
+    nums.add(y)
+
+cnt = 1
+for num in nums:
+    mapper[num] = cnt
+    cnt += 1
+
+for x, y in points:
+    psum[mapper[x]][mapper[y]] += 1
+
+for i in range(1, cnt+1):
+    for j in range(1, cnt+1):
+        psum[i][j] += psum[i-1][j] + psum[i][j-1] - psum[i-1][j-1]
+
+for r, c, x, y in queries:
+    nr = get_lower_bound(r)
+    nc = get_lower_bound(c)
+    nx = get_upper_bound(x)
+    ny = get_upper_bound(y)
+    ans = psum[nx][ny] - psum[nx][nc-1] - psum[nr-1][ny] + psum[nr-1][nc-1]
+    print(ans)
