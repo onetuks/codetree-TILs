9a664361b97241fd603dd2f87faedb3d1d1687cf
Se0 <54990890+onetuks@users.noreply.github.com>
2025-05-02 16:46:43 +0900

    Title: 점 개수 세기, Time: 1017ms, Memory: 55MB, Status: Time Limit Exceed - Codetree

--- 250502/점 개수 세기/count-number-of-points.py
+++ 250502/점 개수 세기/count-number-of-points.py
@@ -9,6 +9,30 @@ mapper = dict()
 points = SortedSet(list(map(int, input().split())))
 queries = [tuple(map(int, input().split())) for _ in range(q)]
 
+def get_lower_bound(num):
+    idx = -1
+    l, r = 0, len(points) - 1
+    while l <= r:
+        m = (l + r) // 2
+        if points[m] >= num:
+            r = m - 1
+            idx = m
+        else:
+            l = m + 1
+    return idx
+
+def get_upper_bound(num):
+    idx = -1
+    l, r = 0, len(points) - 1
+    while l <= r:
+        m = (l + r) // 2
+        if points[m] <= num:
+            l = m + 1
+            idx = m
+        else:
+            r = m - 1
+    return idx
+
 cnt = 1
 for p in points:
     mapper[p] = cnt
@@ -18,8 +42,13 @@ for p in points:
 for i in range(1, n+1):
     psum[i] += psum[i-1]
 
+# print(points)
+# print(psum)
+
 for a, b in queries: 
-    na = points.bisect_left(a) + 1
-    nb = points.bisect_right(b)
-    ans = psum[nb] - psum[na-1]
+    na = get_lower_bound(a)
+    nb = get_upper_bound(b) + 1
+    # print(a,b)
+    # print(na, nb)
+    ans = psum[nb] - psum[na]
     print(ans)
