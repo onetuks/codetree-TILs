24ceace1522e6c4965e3f712482fb380bdafc16c
Se0 <54990890+onetuks@users.noreply.github.com>
2025-05-02 22:32:38 +0900

    Title: 미래가 보이는 가위바위보, Time: 79ms, Memory: 18MB, Status: Runtime Error - Codetree

--- 250502/미래가 보이는 가위바위보/rock-paper-scissors-to-see-the-future.py
+++ 250502/미래가 보이는 가위바위보/rock-paper-scissors-to-see-the-future.py
@@ -1,45 +1,25 @@
-from sys import stdin
+N = int(input())
+shapes = [input().strip() for _ in range(n)]
 
-input = stdin.readline
+L = [0] * n
+R = [0] * n
 
-n = int(input())
-b = ["T"] + [input().strip() for _ in range(n)] + ["T"]
+for shape in "PHS":
+    same_cnt = 0
+    for i in range(N):
+        if shapes[i] == shape:
+            same_cnt += 1
+        L[i] = max(L[i], same_cnt)
 
-lh, rh = [0] * (n + 2), [0] * (n + 2)
-ls, rs = [0] * (n + 2), [0] * (n + 2)
-lp, rp = [0] * (n + 2), [0] * (n + 2)
-
-for i in range(1, n+1):
-    lh[i] = lh[i-1] + (1 if b[i] == "S" else 0)
-    ls[i] = ls[i-1] + (1 if b[i] == "P" else 0)
-    lp[i] = lp[i-1] + (1 if b[i] == "H" else 0)
-
-for i in reversed(range(1, n+1)):
-    rh[i] = rh[i+1] + (1 if b[i] == "S" else 0)
-    rs[i] = rs[i+1] + (1 if b[i] == "P" else 0)
-    rp[i] = rp[i+1] + (1 if b[i] == "H" else 0)
-
-# print(lh)
-# print(ls)
-# print(lp)
-# print(rh)
-# print(rs)
-# print(rp)
+for shape in "PHS":
+    same_cnt = 0
+    for i in reversed(range(N)):
+        if shapes[i] == shape:
+            same_cnt += 1
+        R[i] = max(R[i], same_cnt)
 
 ans = 0
-for i in range(1, n+1):
-    # print("hs", lh[i-1], rs[i], lh[i-1] + rs[i])
-    # print("hp", lh[i-1], rp[i], lh[i-1] + rp[i])
-    # print("sh", ls[i-1], rh[i], ls[i-1] + rh[i])
-    # print("sp", ls[i-1], rp[i], ls[i-1] + rp[i])
-    # print("ph", lp[i-1], rh[i], lp[i-1] + rh[i])
-    # print("ps", lp[i-1], rs[i], lp[i-1] + rs[i])
-    cnt = max(
-        lh[i-1] + rs[i], lh[i-1] + rp[i],
-        ls[i-1] + rh[i], ls[i-1] + rp[i],
-        lp[i-1] + rh[i], lp[i-1] + rs[i]
-    )
-    # print(i, cnt)
-    ans = max(ans, cnt)
+for i in range(N - 1):
+    ans = max(ans, L[i] + R[i+1])
 
 print(ans)
