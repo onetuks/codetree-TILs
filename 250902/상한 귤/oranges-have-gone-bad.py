144855d3c47e564150779d5ed41809047d0ae642
Se0 <54990890+onetuks@users.noreply.github.com>
2025-09-02 22:09:22 +0900

    Title: 상한 귤, Time: 123ms, Memory: 21MB, Status: Passed - Codetree

--- 250902/상한 귤/oranges-have-gone-bad.py
+++ 250902/상한 귤/oranges-have-gone-bad.py
@@ -1,60 +1,55 @@
-from enum import Enum
 from collections import deque
 
 
-class OrangeType(Enum):
-    EMPTY = 0
-    NORMAL = 1
-    SPOLIED = 2
-
-
-DP_SPOILED = 0
-DP_EMPTY = -1
-DP_UNREACHABLE = -2
-DP_INIT = 1e9
-
-DLIST = [(0, 1), (0, -1), (1, 0), (-1, 0)]
-
 n, k = map(int, input().split())
+matrix = [list(map(int, input().split())) for _ in range(n)]
+
+s_pos = [
+    (i, j)
+    for i in range(n)
+    for j in range(n)
+    if matrix[i][j] == 2
+]
+
 q = deque()
-matrix = list()
-dp = [[DP_INIT for _ in range(n)] for _ in range(n)]
+visited = [[False for _ in range(n)] for _ in range(n)]
+step = [[0 for _ in range(n)] for _ in range(n)]
+
+def in_range(i, j):
+    return 0 <= i < n and 0 <= j < n
+
+def can_go(i, j):
+    return in_range(i, j) and not visited[i][j] and matrix[i][j]
+
+def push(i, j, new_step):
+    q.append((i, j))
+    visited[i][j] = True
+    step[i][j] = new_step
+
+def bfs():
+    while q:
+        i, j = q.popleft()
+
+        for dx, dy in [(0, 1), (0, -1), (1, 0), (-1, 0)]:
+            di, dj = i + dx, j + dy
+            if can_go(di, dj):
+                push(di, dj, step[i][j] + 1)
+
+
+for i, j in s_pos:
+    push(i, j, 0)
+
+bfs()
 
 for i in range(n):
-    lst = list(map(int, input().split()))
-    matrix.append(lst)
-    for j, l in enumerate(lst):
-        if l == OrangeType.EMPTY.value:
-            dp[i][j] = DP_EMPTY
-        elif l == OrangeType.SPOLIED.value:
-            dp[i][j] = DP_SPOILED
-            q.append((i, j))
-
-def reachable(i, j):
-    return 0 <= i < n and 0 <= j < n and matrix[i][j] == OrangeType.NORMAL.value
-
-def regulate_dp():
-    for i in range(n):
-        for j in range(n):
-            if dp[i][j] == DP_INIT:
-                dp[i][j] = DP_UNREACHABLE
-
-def infect_orange():
-    # print_answer()
-    while q:
-        i, j = q.pop()
-
-        for dx, dy in DLIST:
-            di, dj = i + dx, j + dy
-            if reachable(di, dj) and dp[di][dj] > dp[i][j] + 1:
-                dp[di][dj] = dp[i][j] + 1
-                q.append((di, dj))
-
-    regulate_dp()
-
-def print_answer():
-    for d in dp:
-        print(*d)
-
-infect_orange()
-print_answer()
+    for j in range(n):
+        if matrix[i][j] == 0:
+            print(-1, end=" ")
+        elif matrix[i][j] == 2:
+            print(0, end=" ")
+        else:
+            if not visited[i][j]:
+                print(-2, end=" ")
+            else:
+                print(step[i][j], end=" ")
+    print()
