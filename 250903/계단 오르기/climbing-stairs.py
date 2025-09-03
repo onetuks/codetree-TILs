4f9464ae8fc88a70f0bfd5f4a655cd727dc13dbc
Se0 <54990890+onetuks@users.noreply.github.com>
2025-09-03 23:02:21 +0900

    Title: 계단 오르기, Time: 47ms, Memory: 16MB, Status: Passed - Codetree

--- 250903/계단 오르기/climbing-stairs.py
+++ 250903/계단 오르기/climbing-stairs.py
@@ -1,9 +1,12 @@
+MOD = 10007
+
 n = int(input())
 
 dp = [0 for _ in range(n * 2)]
 dp[2] = dp[3] = 1
 
 for i in range(4, n + 1):
-    dp[i] = dp[i - 2] + dp[i - 3]
+    dp[i] = (dp[i - 2] + dp[i - 3]) % MOD
 
 print(dp[n])
+# print(dp)
