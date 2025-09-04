8a7e0ad43a15a09057d2cdb6dee3eeae47982d40
Se0 <54990890+onetuks@users.noreply.github.com>
2025-09-04 21:35:41 +0900

    Title: 사각형 채우기 3, Time: 57ms, Memory: 17MB, Status: Passed - Codetree

--- 250904/사각형 채우기 3/rectangle-fill-3.py
+++ 250904/사각형 채우기 3/rectangle-fill-3.py
@@ -3,12 +3,15 @@ MOD = 1_000_000_007
 n = int(input())
 
 dp = [0 for _ in range(n + 2)]
+dp[0] = 1
 dp[1] = 2
 dp[2] = 7
 
 for i in range(3, n + 1):
-    before = sum(dp[j] for j in range(i-2))
-    print(i, before)
-    dp[i] = (dp[i-1]*2 + dp[i-2]*3 - 2*before) % MOD
+    dp[i] = (
+        dp[i - 1] * 2 
+        + dp[i - 2] * 3 
+        + 2 * sum(dp[j] for j in range(i-2))
+        ) % MOD
 
 print(dp[n])
