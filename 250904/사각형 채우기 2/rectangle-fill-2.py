82a251c3aab78258cf9eac1cc69ebdd7c9de9533
Se0 <54990890+onetuks@users.noreply.github.com>
2025-09-04 22:00:28 +0900

    Title: 사각형 채우기 2, Time: 49ms, Memory: 16MB, Status: Passed - Codetree

--- 250904/사각형 채우기 2/rectangle-fill-2.py
+++ 250904/사각형 채우기 2/rectangle-fill-2.py
@@ -1,3 +1,5 @@
+MOD = 10_007
+
 n = int(input())
 
 dp = [0 for i in range(n + 2)]
@@ -5,6 +7,6 @@ dp[1] = 1
 dp[2] = 3
 
 for i in range(3, n + 1):
-    dp[i] = dp[i - 1] + dp[i - 2] * 2
+    dp[i] = (dp[i - 1] + dp[i - 2] * 2) % MOD
 
 print(dp[n])
