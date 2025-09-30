5157644424a6f4a31864f0887404e7e086d76ed2
Se0 <54990890+onetuks@users.noreply.github.com>
2025-09-30 18:00:11 +0900

    Title: 정수 사각형 최솟값의 최대, Time: 67ms, Memory: 17MB, Status: Passed - Codetree

--- 250930/정수 사각형 최솟값의 최대/maximin-path-in-square.py
+++ 250930/정수 사각형 최솟값의 최대/maximin-path-in-square.py
@@ -20,6 +20,12 @@ def init():
     
 init()
 
+'''
+최솟값의 최댓갑
+dp[i][j]: (i, j) 지점에서의 최솟값의 최댓값
+-> min(이전 경로까지의 최댓값, 현재 값)
+-> min(max(왼쪽, 위쪽), 현재값))
+'''
 for i in range(1, n):
     for j in range(1, n):
         dp[i][j] = min(max(dp[i-1][j], dp[i][j-1]), a[i][j])
