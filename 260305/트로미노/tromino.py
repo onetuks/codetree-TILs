e55b234d36fa272e5b728844bafb3c5eb3d2e418
Gamja-rani <54990890+onetuks@users.noreply.github.com>
2026-03-05 11:56:32 +0900

    Title: 트로미노, Time: 44ms, Memory: 16MB, Status: Wrong Answer - Codetree

---
+++ 260305/트로미노/tromino.py
@@ -0,0 +1,29 @@
+DLIST = [
+    [(0, 0), (-1, 0), (0, 1)], 
+    [(0, 0), (0, 1), (1, 0)], 
+    [(0, 0), (1, 0), (0, -1)],
+    [(0, 0), (0, -1), (-1, 0)], 
+    [(0, 1), (0, 2), (0, 3)], 
+    [(1, 0), (2, 0), (3, 0)]]
+
+n, m = map(int, input().split())
+grid = [list(map(int, input().split())) for _ in range(n)]
+
+answer = 0
+
+def out_of_range(i, j):
+    return 0 > i or i >= n or 0 > j or j >= n
+
+for i in range(n):
+    for j in range(n):
+        for dlist in DLIST:
+            sumof = 0
+            for dx, dy in dlist:
+                di, dj = i + dx, j + dy
+                if out_of_range(di, dj):
+                    sumof = 0
+                    break
+                sumof += grid[di][dj]
+            answer = max(answer, sumof)
+
+print(answer)
