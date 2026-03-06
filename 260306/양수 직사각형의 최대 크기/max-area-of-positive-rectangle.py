a071d834ebc985d7eebb5924c5ffa7d79c49b96a
Gamja-rani <54990890+onetuks@users.noreply.github.com>
2026-03-06 17:52:05 +0900

    Title: 양수 직사각형의 최대 크기, Time: 45ms, Memory: 16MB, Status: Wrong Answer - Codetree

---
+++ 260306/양수 직사각형의 최대 크기/max-area-of-positive-rectangle.py
@@ -0,0 +1,23 @@
+n, m = map(int, input().split())
+grid = [list(map(int, input().split())) for _ in range(n)]
+
+ans = 0
+
+def all_positive(x1, y1, x2, y2):
+    for i in range(x1, x2 + 1):
+        for j in range(y1, y2 + 1):
+            if grid[i][j] < 0:
+                return False
+    return True
+
+def get_size(x1, y1, x2, y2):
+    return (x2 + 1 - x1) * (y2 + 1 - y1)
+
+for x1 in range(n):
+    for y1 in range(m):
+        for x2 in range(x1, n):
+            for y2 in range(y1, n):
+                if all_positive(x1, y1, x2, y2):
+                    ans = max(ans, get_size(x1, y1, x2, y2))
+
+print(ans)
