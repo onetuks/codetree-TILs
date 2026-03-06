f07d5a9c64c17825a5c3ab1351250dfe46abbf63
Gamja-rani <54990890+onetuks@users.noreply.github.com>
2026-03-06 15:33:05 +0900

    Title: 금 채굴하기, Time: 71ms, Memory: 17MB, Status: Wrong Answer - Codetree

--- 260306/금 채굴하기/gold-mining.py
+++ 260306/금 채굴하기/gold-mining.py
@@ -1,50 +1,24 @@
-from collections import deque
-
-DLIST = [[-1, 0], [0, -1], [1, 0], [0, 1]]
-
 n, m = map(int, input().split())
 grid = [list(map(int, input().split())) for _ in range(n)]
 
 ans = 0
 
-def out_of_range(i, j):
-    return 0 > i or i >= n or 0 > j or j >= n
-
 def is_profit(k, c):
     return k ** 2 + (k + 1) ** 2 <= c * m
 
-def bfs(i, j, k):
-    visited = [[False for _ in range(n)] for _ in range(n)]
-
-    q = deque([(i, j, 0)])
-    visited[i][j] = True
-
-    cnt = 0
-
-    while q:
-        x, y, s = q.popleft()
-        cnt += grid[x][y]
-
-        if s >= k:
-            continue
-
-        for dx, dy in DLIST:
-            di, dj = x + dx, y + dy
-            if out_of_range(di, dj) or visited[di][dj]:
-                continue
-            visited[di][dj] = True
-            q.append((di, dj, s + 1))
-
-    return cnt
-
-# print(bfs(2, 2, 2))
+def get_cnt(i, j, k):
+    return sum([
+        grid[x][y]
+        for x in range(n)
+        for y in range(n)
+        if abs(i - x) + abs(j - y) <= k 
+    ])
 
 for i in range(n):
     for j in range(n):
         for k in range(n):
-            cnt = bfs(i, j, k)
+            cnt = get_cnt(i, j, k)
             if is_profit(k, cnt):
                 ans = max(ans, cnt)
 
 print(ans)
-        
