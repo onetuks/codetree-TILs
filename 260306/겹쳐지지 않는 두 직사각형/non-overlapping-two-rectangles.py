969d9b777dae29b19b536fc85dba2928440499b6
Gamja-rani <54990890+onetuks@users.noreply.github.com>
2026-03-06 16:59:38 +0900

    Title: 겹쳐지지 않는 두 직사각형, Time: 49ms, Memory: 16MB, Status: Wrong Answer - Codetree

--- 260306/겹쳐지지 않는 두 직사각형/non-overlapping-two-rectangles.py
+++ 260306/겹쳐지지 않는 두 직사각형/non-overlapping-two-rectangles.py
@@ -1,48 +1,56 @@
 n, m = map(int, input().split())
 grid = [list(map(int, input().split())) for _ in range(n)]
 
-ans = -1e9
+def draw(x1, y1, x2, y2, board):
+    for i in range(x1, x2 + 1):
+        for j in range(y1, y2 + 1):
+            board[i][j] += 1
 
-def overlapped(pos1, pos2):
-    visited = [[False for _ in range(n)] for _ in range(n)]
-    si, sj = pos1[0]
-    ei, ej = pos1[1]
-    for i in range(si, ei):
-        for j in range(sj, ej):
-            visited[i][j] = True
-    si, sj = pos2[0]
-    ei, ej = pos2[1]
-    for i in range(si, ei):
-        for j in range(sj, ej):
-            if visited[i][j]:
+def check(board):
+    for i in range(n):
+        for j in range(n):
+            if board[i][j] >= 2:
                 return True
     return False
 
-def get_sum(pos):
-    si, sj = pos[0]
-    ei, ej = pos[1]
+def overlapped(x1, y1, x2, y2, x3, y3, x4, y4):
+    board = [[0 for _ in range(n)] for _ in range(n)]
+    draw(x1, y1, x2, y2, board)
+    draw(x3, y3, x4, y4, board)
+    return check(board)
+
+def rect_sum(x1, y1, x2, y2):
     sumof = 0
-    for i in range(si, ei):
-        for j in range(sj, ej):
+    for i in range(x1, x2 + 1):
+        for j in range(y1, y2 + 1):
             sumof += grid[i][j]
     return sumof
 
-for si1 in range(n):
-    for sj1 in range(n):
-        for ei1 in range(si1 + 1, n + 1):
-            for ej1 in range(sj1 + 1, n + 1):
-                pos1 = [(si1, sj1), (ei1, ej1)]
-                for si2 in range(n):
-                    for sj2 in range(n):
-                        for ei2 in range(si2 + 1, n + 1):
-                            for ej2 in range(sj2 + 1, n + 1):
-                                pos2 = [(si2, sj2), (ei2, ej2)]
-                                if overlapped(pos1, pos2):
-                                    continue
-                                sumof1 = get_sum(pos1)
-                                sumof2 = get_sum(pos2)
-                                sumof = sumof1 + sumof2
-                                ans = max(ans, sumof)
+def find_max_sum_rect(x1, y1, x2, y2):
+    max_sum = -1e9
+    for i in range(n):
+        for j in range(n):
+            for k in range(i, n):
+                for l in range(j, n):
+                    if overlapped(x1, y1, x2, y2, i, j, k, l):
+                        continue
+                    max_sum = max(
+                        max_sum,
+                        rect_sum(x1, y1, x2, y2) + 
+                        rect_sum(i, j, k, l)
+                    )
+    return max_sum
 
-print(ans)
+def find_max_sum():
+    max_sum = -1e9
 
+    for i in range(n):
+        for j in range(n):
+            for k in range(i, n):
+                for l in range(j, n):
+                    max_sum = max(max_sum, find_max_sum_rect(i, j, k, l))
+    
+    return max_sum
+
+
+print(find_max_sum())
