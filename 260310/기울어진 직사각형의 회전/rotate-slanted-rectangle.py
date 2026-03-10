e63a8b4f94d60908cd465a0d1df10ad8d4f33de3
Gamja-rani <54990890+onetuks@users.noreply.github.com>
2026-03-10 21:04:37 +0900

    Title: 기울어진 직사각형의 회전, Time: 48ms, Memory: 16MB, Status: Wrong Answer - Codetree

---
+++ 260310/기울어진 직사각형의 회전/rotate-slanted-rectangle.py
@@ -0,0 +1,40 @@
+n = int(input())
+grid = [list(map(int, input().split())) for _ in range(n)]
+r, c, m1, m2, m3, m4, dir = map(int, input().split())
+
+r -= 1
+c -= 1
+
+def rotate_counter():
+    global r, c, m1, m2, m3, m4
+    temp = grid[r][c]
+    for i in range(m4):
+        grid[r][c] = grid[r - 1][c - 1]
+        r -= i
+        c -= i
+    for i in range(m3):
+        grid[r][c] = grid[r - 1][c + 1]
+        r -= i
+        c += i
+    # for i in range(m2):
+    #     grid[r][c] = grid[r + 1][c + 1]
+    #     r += i
+    #     c += i
+    # for i in range(m1):
+    #     grid[r][c] = grid[r + 1][c - 1]
+    #     r += i
+    #     c -= i
+    # grid[r - 1][c + 1] = temp
+
+def rotate_clock():
+    pass
+
+if dir == 0: # 반시계
+    rotate_counter()
+else:
+    rotate_clock()
+
+for row in grid:
+    for item in row:
+        print(item, end = " ")
+    print()
