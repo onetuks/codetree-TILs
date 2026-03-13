8911a62aac7bee865a7eed4122d213869e9a6511
Gamja-rani <54990890+onetuks@users.noreply.github.com>
2026-03-13 13:47:22 +0900

    Title: 벽 짚고 미로 탈출하기, Time: 469ms, Memory: 80MB, Status: Memory Limit Exceed - Codetree

--- 260313/벽 짚고 미로 탈출하기/escape-maze-with-wall-following.py
+++ 260313/벽 짚고 미로 탈출하기/escape-maze-with-wall-following.py
@@ -12,38 +12,43 @@ dlist = [[0, 1], [1, 0], [0, -1], [-1, 0]]
 ans = -1
 block = -100
 
-def no_right_block(i, j, d):
-    di, dj = i + dlist[d][0], j + dlist[d][1]
+def no_right_block(i, j, curr_dir):
+    right_dir = (curr_dir + 1) % 4
+    di, dj = i + dlist[right_dir][0], j + dlist[right_dir][1]
     if 0 <= di < n and 0 <= dj < n:
         return grid[di][dj] != '#'
     return False
 
+def out_of_range(i, j):
+    return 0 > i or i >= n or 0 > j or j >= n
+
+def no_straight_block(i, j, d):
+    di, dj = i + dlist[d][0], j + dlist[d][1]
+    if out_of_range(di, dj):
+        return True
+    return grid[di][dj] != '#'
+
 def dfs(i, j, dir):
-    global ans
-
-    print(i, j, dir)
-
-    if 0 > i or i >= n or 0 > j or j >= n:
-        print("out of range")
+    # print(i, j, dir)
+    if out_of_range(i, j):
         return 0
-    elif grid[i][j] == '#':
-        return block
-    
     if no_right_block(i, j, dir):
-        print("turn clock wise")
         d = (dir + 1) % 4
-        return dfs(i + dlist[d][0] , j + dlist[d][1], d)
+        di, dj = i + dlist[d][0], j + dlist[d][1]
+        cnt = dfs(di, dj, d)
+        if cnt == -1:
+            return cnt
+        return cnt + 1
     for idx in range(4):
         d = (dir - idx + 4) % 4
-        if dir == d:
-            print("go straight")
-        else:
-            print("turn counter wise")
-        cnt = dfs(i + dlist[d][0], j + dlist[d][1], d)
-        if cnt != block:
-            return cnt
+        if no_straight_block(i, j, d):
+            di, dj = i + dlist[d][0], j + dlist[d][1]
+            cnt = dfs(di, dj, d)
+            if cnt == -1:
+                return cnt
+            return cnt + 1
     return -1
 
-dfs(x, y, 0)
+ans = dfs(x, y, 0)
 
 print(ans)
