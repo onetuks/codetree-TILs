27f8f7609706e1125a662b8e08bbc64f1551daf1
Gamja-rani <54990890+onetuks@users.noreply.github.com>
2026-03-17 13:37:37 +0900

    Title: 뱀은 사과를 좋아해, Time: 53ms, Memory: 16MB, Status: Wrong Answer - Codetree

--- 260317/뱀은 사과를 좋아해/snake-loves-apples.py
+++ 260317/뱀은 사과를 좋아해/snake-loves-apples.py
@@ -8,29 +8,55 @@ DMAP = {
     "R": 3
 }
 
-N, M, K = map(int, input().split())
+n, m, k = map(int, input().split())
 
 locations = [tuple(map(int, input().split())) for _ in range(m)]
-
-print(locations) 
+commands = [tuple(input().split()) for _ in range(k)]
 
 class Snake:
     def __init__(self):
-        self.q = deque([(1, 1)])
+        self.time = 0
+        self.queue = deque([(1, 1)])
+
+    def _out_of_range(self, i, j):
+        return 1 > i or i > n or 1 > j or j > n
+
+    def _overlapped(self, i, j):
+        for x, y in self.queue:
+            if i == x and j == y:
+                return True
+        return False
+
+    def _apple_exists(self, i, j):
+        for x, y in locations:
+            if i == x and j == y:
+                return True
+        return False
 
     def move(self, dir, cnt):
-        cx, cy = self.q[0]
+        cx, cy = self.queue[0]
         for i in range(1, cnt + 1):
+            self.time += 1
             di = cx + DLIST[DMAP[dir]][0] * i
             dj = cy + DLIST[DMAP[dir]][1] * i
-            if out_of_range(di, dj):
+            if self._out_of_range(di, dj):
                 return False
-            elif overlapped(di, dj):
+            elif self._overlapped(di, dj):
                 return False
 
-            if apple_exists(di, dj):
-                self.q.appendleft((di, dj))
-                x.remove
-            self.q.appendleft((di, dj))
-            self.q.pop()
+            if self._apple_exists(di, dj):
+                self.queue.appendleft((di, dj))
+                locations.remove((di, dj))
+            else:
+                self.queue.appendleft((di, dj))
+                self.queue.pop()
         return True
+
+snake = Snake()
+for dir, cnt in commands:
+    cnt = int(cnt)
+    result = snake.move(dir, cnt)
+    if not result:
+        break
+
+print(snake.time)
