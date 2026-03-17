6cb82de80796347464aff35c20b137918835a923
Gamja-rani <54990890+onetuks@users.noreply.github.com>
2026-03-17 14:13:42 +0900

    Title: 핀볼게임, Time: 45ms, Memory: 16MB, Status: Passed - Codetree

--- 260317/핀볼게임/pinball-game.py
+++ 260317/핀볼게임/pinball-game.py
@@ -1,74 +1,32 @@
 n = int(input())
 grid = [list(map(int, input().split())) for _ in range(n)]
 
-# U D L R
 DLIST = [[-1, 0], [1, 0], [0, -1], [0, 1]]
 
-class Pair:
-    def __init__(self, i, j, dir):
-        self.i = i
-        self.j = j
-        self.dir = dir
+def in_range(i, j):
+    return 0 <= i < n and 0 <= j < n
 
-    def _out_of_range(self):
-        return 0 > self.i or self.i >= n or 0 > self.j or self.j >= n
-
-    def _is_one_block(self):
-        return grid[self.i][self.j] == 1
+def calc(x, y, dir):
+    time = 1
+    i, j = x, y
+    while in_range(i, j):
+        if grid[i][j] == 1:
+            dir = 3 - dir
+        elif grid[i][j] == 2:
+            dir = (dir + 2) if dir < 2 else (dir - 2)
+        
+        i += DLIST[dir][0]
+        j += DLIST[dir][1]
+        time += 1
     
-    def _is_two_block(self):
-        return grid[self.i][self.j] == 2
-
-    def move(self):
-        self.i += DLIST[self.dir][0]
-        self.j += DLIST[self.dir][1]
-        if self._out_of_range():
-            return False
-        # U(0) -> R(3) / R(3) -> U(0) / D(1) -> L(2) / L(2) -> D(1)
-        if self._is_one_block():
-            # print("one")
-            if self.dir == 0:
-                self.dir = 3
-            elif self.dir == 1:
-                self.dir = 2
-            elif self.dir == 2:
-                self.dir = 1
-            elif self.dir == 3:
-                self.dir = 0
-        # U(0) <-> L(2) / D(1) <-> R(3)
-        elif self._is_two_block():
-            # print("two")
-            if self.dir == 0:
-                self.dir = 2
-            elif self.dir == 1:
-                self.dir = 3
-            elif self.dir == 2:
-                self.dir = 0
-            elif self.dir == 3:
-                self.dir = 1
-        return True
-
-    def __str__(self):
-        return "(" + str(self.i) + str(self.j) + ") - " + str(self.dir)
-
-commands = []
-for i in range(n):
-    commands.append((i, 0, 3)) # left
-    commands.append((i, n - 1, 2)) # right
-    commands.append((0, i, 1)) # up
-    commands.append((n - 1, i, 0)) # down
+    return time
+        
 
 ans = 0
-
-for i, j, dir in commands:
-    pair = Pair(i, j, dir)
-    time = 1
-    while True:
-        time += 1
-        if not pair.move():
-            break
-        # print(pair)
-    # print()
-    ans = max(ans, time)
+for i in range(n):
+    ans = max(ans, calc(n - 1, i, 0))
+    ans = max(ans, calc(0, i, 1))
+    ans = max(ans, calc(i, n - 1, 2))
+    ans = max(ans, calc(i, 0, 3))
 
 print(ans)
