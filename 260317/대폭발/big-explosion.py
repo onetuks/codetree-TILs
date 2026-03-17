698104debfe0c2b2f02f463ce86c2953c7359765
Gamja-rani <54990890+onetuks@users.noreply.github.com>
2026-03-17 13:12:40 +0900

    Title: 대폭발, Time: 72ms, Memory: 18MB, Status: Wrong Answer - Codetree

---
+++ 260317/대폭발/big-explosion.py
@@ -0,0 +1,49 @@
+dlist = [[-1, 0], [1, 0], [0, -1], [0, 1]]
+
+n, m, r, c = map(int, input().split())
+
+r -= 1
+c -= 1
+
+grid = [[False for _ in range(n)] for _ in range(n)]
+grid[r][c] = True
+temp = [[False for _ in range(n)] for _ in range(n)]
+
+def save_grid():
+    global temp
+    temp = [[False for _ in range(n)] for _ in range(n)]
+    for i in range(n): 
+        for j in range(n):
+            temp[i][j] = grid[i][j]
+
+def load_grid():
+    for i in range(n):
+        for j in range(n):
+            grid[i][j] = temp[i][j]
+
+def print_grid():
+    for g in grid:
+        print(g)
+    print()
+
+def spread(i, j, t):
+    for dx, dy in dlist:
+        di = i + dx * t
+        dj = j + dy * t
+        if 0 <= di < n and 0 <= dj < n:
+            temp[di][dj] = True
+
+for t in range(1, m + 1):
+    save_grid()
+    for i in range(n):
+        for j in range(n):
+            if grid[i][j]:
+                spread(i, j, t)
+    load_grid()
+    # print_grid()
+            
+ans = 0
+for i in range(n):
+    ans += sum(grid[i])
+
+print(ans)
